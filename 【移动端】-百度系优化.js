4dfee24e7ac5a20ed3d9fd559fb5267da45c958b
WhiteSev <893177236@qq.com>
2023-01-12 11:57:42 +0800

    更新工具类版本

--- 【移动端】-百度系优化.js
+++ 【移动端】-百度系优化.js
@@ -30,7 +30,7 @@
 // @grant        GM_xmlhttpRequest
 // @require	     https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.4.1/jquery.min.js
 // @require      https://greasyfork.org/scripts/449471-viewer/code/Viewer.js?version=1081056
-// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1137273
+// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1137591
 // @run-at       document-start
 // ==/UserScript==
 
