dc303cf4257ae5ec1c3c7b867aa762dc9dec1604
WhiteSev <893177236@qq.com>
2023-01-21 13:42:57 +0800

    更新一下代码什么的

--- 【移动端】-百度系优化.js
+++ 【移动端】-百度系优化.js
@@ -1,22 +1,25 @@
 // ==UserScript==
 // @name         【移动端】-百度系优化
-// @namespace    http://tampermonkey.net/
-// @version      0.6.3
+// @icon         https://www.baidu.com/favicon.ico
+// @namespace    https://greasyfork.org/zh-CN/scripts/418349-移动端-百度系优化
+// @supportURL   https://greasyfork.org/zh-CN/scripts/418349-移动端-百度系优化/feedback
+// @version      0.6.4
+// @author       WhiteSevs
 // @description  用于【移动端】的百度系列产品优化，包括【百度搜索】、【百家号】、【百度贴吧】、【百度文库】、【百度经验】、【百度百科】、【百度知道】、【百度翻译】、【百度图片】、【百度地图】
-// @include      *://m.baidu.com/*
-// @include      *://www.baidu.com/*
-// @include      *://baijiahao.baidu.com/*
-// @include      *://tieba.baidu.com/*
-// @include      *://wk.baidu.com/*
-// @include      *://tanbi.baidu.com/*
-// @include      *://jingyan.baidu.com/*
-// @include      *://baike.baidu.com/*
-// @include      *://zhidao.baidu.com/*
-// @include      *://fanyi.baidu.com/*
-// @include      *://fanyi-app.baidu.com/*
-// @include      *://image.baidu.com/*
-// @include      *://map.baidu.com/*
-// @include      *://xue.baidu.com/*
+// @match        *://m.baidu.com/*
+// @match        *://www.baidu.com/*
+// @match        *://baijiahao.baidu.com/*
+// @match        *://tieba.baidu.com/*
+// @match        *://wk.baidu.com/*
+// @match        *://tanbi.baidu.com/*
+// @match        *://jingyan.baidu.com/*
+// @match        *://baike.baidu.com/*
+// @match        *://zhidao.baidu.com/*
+// @match        *://fanyi.baidu.com/*
+// @match        *://fanyi-app.baidu.com/*
+// @match        *://image.baidu.com/*
+// @match        *://map.baidu.com/*
+// @match        *://xue.baidu.com/*
 // @connect      www.baidu.com
 // @connect      m.baidu.com
 // @connect      tieba.baidu.com
@@ -35,7 +38,7 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function () {
+https: (function () {
 	"use strict";
 	const LOG = GM_getValue("LOG", false);
 	var __console__ = {};
