15429a48cf3d2e66b1a6b6426c7db962ce68b08c
WhiteSev <893177236@qq.com>
2023-01-31 17:23:28 +0800

    新增listenKeyPress

--- 库/Utils.js
+++ 库/Utils.js
@@ -2,7 +2,7 @@
  * @overview	 自己常用的工具类定义
  * @copyright  GPL-3.0-only
  * @author  WhiteSevs
- * @version  0.9
+ * @version  1.0
  */
 let Utils = {};
 
@@ -40,7 +40,7 @@ Utils.tryCatch = function (func, params, errorFunc) {
 		result = typeof func === "string" ? window.eval(func) : func(params);
 	} catch (error) {
 		console.log(
-			"%c" + (func.name ? func.name : func + "出现错误"),
+			"%c" + (func?.name ? func?.name : func + "出现错误"),
 			"color: #f20000"
 		);
 		console.log("%c" + ("错误原因：" + error), "color: #f20000");
@@ -1711,4 +1711,91 @@ Utils.Hooks = function () {
 		}
 		return true;
 	};
+};
+
+/*
+ * @description 监听某个元素键盘按键事件或window全局按键事件
+ * @param {Window|Node} listenObj - 需要监听的对象，可以是全局Window或者某个元素
+ * @param {function} callback - 自己定义的回调事件，参数1为当前的key，参数2为组合按键，数组类型，包含ctrl、shift、alt和meta（win键或mac的cmd键）
+ * @example Utils.listenKeyPress(window,(keyName,otherKey)=>{
+ * 		if(keyName === "Enter"){
+ * 			console.log("回车按键")
+ * 		}
+ * 		if(otherKey.indexOf("ctrl") && keyName === "Enter" ){
+ * 			console.log("Ctrl和回车键");
+ *    }
+ * })
+ *
+ */
+/* 
+字母和数字键的键码值(keyCode)
+按键	键码	按键	键码	按键	键码	按键	键码
+ A	  65		J		 74		 S		83		1     49
+ B	  66		K		 75		 T		84		2		  50
+ C	  67		L		 76		 U		85		3		  51
+ D	  68		M		 77		 V		86		4		  52
+ E	  69		N		 78		 W		87		5	 	  53
+ F		70		O		 79		 X		88		6		  54
+ G		71		P		 80		 Y		89		7		  55
+ H		72		Q		 81		 Z		90		8		  56
+ I		73		R		 82		 0		48		9		  57
+
+数字键盘上的键的键码值(keyCode)	功能键键码值(keyCode)
+按键	键码	按键  	键码	按键	键码	按键	键码
+ 0		96		8	     104   F1 	112	  F7	 118
+ 1		97		9	     105	 F2 	113	  F8	 119
+ 2		98		*	     106	 F3 	114		F9	 120
+ 3		99		+	     107	 F4 	115		F10	 121
+ 4		100		Enter  108	 F5 	116		F11	 122
+ 5		101		-			 109	 F6 	117		F12	 123
+ 6		102		.			 110	 	 	 	 
+ 7		103		/			 111	 	 
+ 
+控制键键码值(keyCode)
+按键	     键码		 按键	        键码 		按键	         键码		按键		键码
+BackSpace	 8	 		Esc	      	 27	   	 Right Arrow	  39		-_		 189
+Tab	       9	 		Spacebar	 	 32	   	 Dw Arrow				40		.>		 190
+Clear	 		 12			Page Up		   33		 	 Insert					45		/?		 191
+Enter			 13			Page Down		 34		 	 Delete					46		`~		 192
+Shift		   16			End					 35		 	 Num Lock				144		[{		 219
+Control	   17			Home				 36		 	 ;:							186		\|		 220
+Alt		     18			Left Arrow	 37		 	 =+							187		]}		 221
+Cape Lock	 20			Up Arrow	   38		 	 ,<							188		'"		 222
+
+多媒体键码值(keyCode)
+按键		  键码
+音量加	  175	 	 	 	 	 	 
+音量减	  174	 	 	 	 	 	 
+停止	    179	 	 	 	 	 	 
+静音	    173	 	 	 	 	 	 
+浏览器	  172	 	 	 	 	 	 
+邮件	    180	 	 	 	 	 	 
+搜索	    170	 	 	 	 	 	 
+收藏	    171
+*/
+Utils.listenKeyPress = function (listenObj, callback) {
+	if (!(listenObj instanceof Window) || !(listenObj instanceof Node)) {
+		throw "Utils.listenKeyPress 参数 listenObj 必须为 Window|Node 类型";
+	}
+	if (typeof callback !== "function") {
+		throw "Utils.listenKeyPress 参数 callback 必须为 Function 类型";
+	}
+	listenObj.addEventListener("keypress", function (event) {
+		event = event ? event : window.event;
+		let keyName = event.code ? event.code : event.key;
+		let otherCodeList = [];
+		if (event.ctrlKey) {
+			otherCodeList = [...otherCodeList, "ctrl"];
+		}
+		if (event.altKey) {
+			otherCodeList = [...otherCodeList, "alt"];
+		}
+		if (event.metaKey) {
+			otherCodeList = [...otherCodeList, "meta"];
+		}
+		if (event.shiftKey) {
+			otherCodeList = [...otherCodeList, "shift"];
+		}
+		callback(keyName, otherCodeList);
+	});
 };
