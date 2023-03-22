e902cee918a5f2a99a470efea41d2017d103e138
WhiteSev <893177236@qq.com>
2023-03-22 14:59:47 +0800

    #

--- 蛋仔乐消除.js
+++ 蛋仔乐消除.js
@@ -5,7 +5,7 @@
 // @description  自动生成的描述
 // @author       WhiteSev
 // @match        https://party.163.com/h5/20230120/xxl/player/*
-// @require	     https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.4.1/jquery.min.js
+// @require      https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.4.1/jquery.min.js
 // @require      https://greasyfork.org/scripts/462234-message/code/Message.js?version=1164153
 // @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1164713
 // @grant        GM_addStyle
