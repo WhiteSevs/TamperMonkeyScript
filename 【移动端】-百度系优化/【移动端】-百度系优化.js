a73b9ac1ad982d3d8589d37b441e7650c424821a
WhiteSev <893177236@qq.com>
2023-04-08 18:28:58 +0800

    修改Utils版本

--- 【移动端】-百度系优化/【移动端】-百度系优化.js
+++ 【移动端】-百度系优化/【移动端】-百度系优化.js
@@ -37,7 +37,7 @@
 // @grant        unsafeWindowresponse
 // @require	     https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.4.1/jquery.min.js
 // @require      https://greasyfork.org/scripts/449471-viewer/code/Viewer.js?version=1170654
-// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1169937
+// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1172659
 // @run-at       document-start
 // ==/UserScript==
 
