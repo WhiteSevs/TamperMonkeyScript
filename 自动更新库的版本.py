0e5d78302a18c8621d937796e5e244f7d3dc8b8d
WhiteSev <893177236@qq.com>
2023-06-29 09:05:39 +0800

    更新写法

--- 自动更新库的版本.py
+++ 自动更新库的版本.py
@@ -27,8 +27,15 @@ def get_ie_proxy():
         print("获取IE代理失败：", e)
         return None
 
+
 def handleLibraryData(libraryIdList):
     result = []
+    # 调用函数获取IE代理
+    ie_proxy = get_ie_proxy()
+    if ie_proxy:
+        print("IE代理：", ie_proxy)
+    else:
+        print("IE代理未启用")
     for libraryId in libraryIdList:
         httpx_proxy = None
         if ie_proxy:
@@ -50,61 +57,58 @@ def handleLibraryData(libraryIdList):
         })
     return result
 
-# 调用函数获取IE代理
-ie_proxy = get_ie_proxy()
-if ie_proxy:
-    print("IE代理：", ie_proxy)
-else:
-    print("IE代理未启用")
 
-directory = "./"
+def replaceContent(path, dataList):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".js"):
+                file_path = os.path.join(root, file)
+                content = ""
+                replaced_content = ""
+                with open(file=file_path, mode="r", encoding="utf-8", errors="ignore") as f:
+                    content = f.read()
+                replaced_content = content
+                for library in dataList:
+                    pattern = f"""{library["url"]}(\?version=[\d]+|)"""
+                    replacement = f"""{library["url"]}?version={library["version"]}"""
+                    patternMatch = re.findall(
+                        pattern, content)
+                    if len(patternMatch):
+                        replaced_content = re.sub(
+                            pattern, replacement, replaced_content)
+                        findData = [
+                            item for item in patternMatch if item != '']
+                        oldVersion = ""
+                        if len(findData):
+                            oldVersionMatch = re.compile(
+                                r'version=([\d]+)').search(findData[0])
+                            if oldVersion is not None:
+                                oldVersion = oldVersionMatch[0].replace(
+                                    "version=", "")
+                        if oldVersion != library["version"]:
+                            print(
+                                f"""{library["name"]} {len(patternMatch)}处 版本: {oldVersion} => {library["version"]}""")
+                if content != replaced_content:
+                    with open(file=file_path, mode="w", encoding="utf-8", errors="ignore") as f:
+                        f.write(replaced_content)
+                    file_path = re.sub(r'\.\/.+\\', '', file_path)
+                    print("当前文件: "+file_path)
+                    print("")
+                    print("")
 
-libraryList = handleLibraryData([
-    "449471",
-    "449512",
-    "449562",
-    "452322",
-    "455186",
-    "456470",
-    "456485",
-    "456607",
-    "462234",
-    "465550",
-    "465772"
-])
 
-for root, dirs, files in os.walk(directory):
-    for file in files:
-        if file.endswith(".js"):
-            file_path = os.path.join(root, file)
-            content = ""
-            replaced_content = ""
-            with open(file=file_path, mode="r", encoding="utf-8", errors="ignore") as f:
-                content = f.read()
-            replaced_content = content
-            for library in libraryList:
-                pattern = f"""{library["url"]}(\?version=[\d]+|)"""
-                replacement = f"""{library["url"]}?version={library["version"]}"""
-                patternMatch = re.findall(
-                    pattern, content)
-                if len(patternMatch):
-                    replaced_content = re.sub(
-                        pattern, replacement, replaced_content)
-                    findData = [item for item in patternMatch if item != '']
-                    oldVersion = ""
-                    if len(findData):
-                        oldVersionMatch = re.compile(
-                            r'version=([\d]+)').search(findData[0])
-                        if oldVersion is not None:
-                            oldVersion = oldVersionMatch[0].replace(
-                                "version=", "")
-                    if oldVersion != library["version"]:
-                        print(
-                            f"""{library["name"]} {len(patternMatch)}处 版本: {oldVersion} => {library["version"]}""")
-            if content != replaced_content:
-                with open(file=file_path, mode="w", encoding="utf-8", errors="ignore") as f:
-                    f.write(replaced_content)
-                file_path = re.sub(r'\.\/.+\\', '', file_path)
-                print("当前文件: "+file_path)
-                print("")
-                print("")
+if __name__ == "__main__":
+    libraryList = handleLibraryData([
+        "449471",
+        "449512",
+        "449562",
+        "452322",
+        "455186",
+        "456470",
+        "456485",
+        "456607",
+        "462234",
+        "465550",
+        "465772"
+    ])
+    replaceContent("./", libraryList)
