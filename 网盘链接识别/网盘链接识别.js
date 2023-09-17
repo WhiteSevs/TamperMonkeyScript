91985ab523547dab65b789976656a18fb1ea7d4a
WhiteSev <893177236@qq.com>
2023-09-17 10:15:16 +0800

    使用utils.setClip代替GM_setClipboard； 去除权限@grant GM_setClipboard； 去除权限@grant GM_openTab；

--- 网盘链接识别/网盘链接识别.js
+++ 网盘链接识别/网盘链接识别.js
@@ -2,7 +2,7 @@
 // @name         网盘链接识别
 // @namespace    https://greasyfork.org/zh-CN/scripts/445489-网盘链接识别
 // @supportURL   https://greasyfork.org/zh-CN/scripts/445489-网盘链接识别/feedback
-// @version      23.9.16.18.00
+// @version      23.9.17.10.00
 // @description  识别网页中显示的网盘链接，目前包括百度网盘、蓝奏云、天翼云、中国移动云盘(原:和彩云)、阿里云、文叔叔、奶牛快传、123盘、腾讯微云、迅雷网盘、115网盘、夸克网盘、城通网盘(部分)、坚果云、BT磁力，支持蓝奏云、天翼云(需登录)、123盘、奶牛和坚果云(需登录)直链获取下载，页面动态监控加载的链接
 // @author       WhiteSevs
 // @match        *://*/*
@@ -10,11 +10,9 @@
 // @license      GPL-3.0-only
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @grant        GM_setClipboard
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // @grant        GM_info
-// @grant        GM_openInTab
 // @grant        GM_registerMenuCommand
 // @grant        GM_unregisterMenuCommand
 // @grant        unsafeWindow
@@ -58,7 +56,7 @@
 // @require      https://greasyfork.org/scripts/456470-%E7%BD%91%E7%9B%98%E9%93%BE%E6%8E%A5%E8%AF%86%E5%88%AB-%E5%9B%BE%E6%A0%87%E5%BA%93/code/%E7%BD%91%E7%9B%98%E9%93%BE%E6%8E%A5%E8%AF%86%E5%88%AB-%E5%9B%BE%E6%A0%87%E5%BA%93.js?version=1211345
 // @require      https://greasyfork.org/scripts/465550-js-%E5%88%86%E9%A1%B5%E6%8F%92%E4%BB%B6/code/JS-%E5%88%86%E9%A1%B5%E6%8F%92%E4%BB%B6.js?version=1249092
 // @require      https://greasyfork.org/scripts/456485-pops/code/pops.js?version=1249088
-// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1250551
+// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1251508
 // @require      https://greasyfork.org/scripts/465772-domutils/code/DOMUtils.js?version=1250683
 // ==/UserScript==
 
@@ -3051,7 +3049,7 @@
       accessCode,
       toastText = "提取码已复制"
     ) {
-      GM_setClipboard(accessCode);
+      utils.setClip(accessCode);
       GM_setValue("tempNetDiskInfo", {
         shareCode: shareCode,
         accessCode: accessCode,
@@ -3074,7 +3072,7 @@
       accessCode,
       toastText = "已复制"
     ) {
-      GM_setClipboard(
+      utils.setClip(
         NetDiskParse.getBlankUrl(
           netDiskName,
           netDiskIndex,
