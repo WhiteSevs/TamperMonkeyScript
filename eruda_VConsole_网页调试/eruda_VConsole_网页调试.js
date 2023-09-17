e360263a3434170b35e8f3e5b03b2cd003431c18
WhiteSev <893177236@qq.com>
2023-09-17 16:21:11 +0800

    更新库版本

--- eruda_VConsole_网页调试/eruda_VConsole_网页调试.js
+++ eruda_VConsole_网页调试/eruda_VConsole_网页调试.js
@@ -13,7 +13,7 @@
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @grant           GM_deleteValue
-// @require         https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1251638
+// @require         https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1251653
 // ==/UserScript==
 
 (function () {
