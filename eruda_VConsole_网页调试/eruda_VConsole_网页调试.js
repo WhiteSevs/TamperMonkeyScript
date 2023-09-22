6c1287c45141af6008fe44a3c96da43accd87a7b
WhiteSev <893177236@qq.com>
2023-09-22 12:26:35 +0800

    更新Utils库

--- eruda_VConsole_网页调试/eruda_VConsole_网页调试.js
+++ eruda_VConsole_网页调试/eruda_VConsole_网页调试.js
@@ -13,7 +13,7 @@
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @grant           GM_deleteValue
-// @require         https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1253979
+// @require         https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1254410
 // ==/UserScript==
 
 (function () {
