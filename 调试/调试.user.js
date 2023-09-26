9145f0ffc89820792ee5722566db612622ac2d60
WhiteSev <893177236@qq.com>
2023-09-26 18:18:05 +0800

    #

--- 调试/调试.user.js
+++ 调试/调试.user.js
@@ -35,7 +35,7 @@
 // @grant        GM.ChromeXt
 // @connect      *
 // @run-at       document-start
-// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1256406
+// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1256418
 // @require      https://greasyfork.org/scripts/465772-domutils/code/DOMUtils.js?version=1256298
 // ==/UserScript==
 
