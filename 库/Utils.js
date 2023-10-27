07fc7814540b668d5322323ef5e3c89eba807904
WhiteSev <893177236@qq.com>
2023-10-27 11:50:35 +0800

    修正Utils.GM_Menu的getShowTextValue获取为undefined问题

--- 库/Utils.js
+++ 库/Utils.js
@@ -22,7 +22,7 @@
   /**
    * @type {string} 工具类的版本
    */
-  Utils.version = "2023-10-26";
+  Utils.version = "2023-10-27";
   /**
    * JSON数据从源端替换到目标端中，如果目标端存在该数据则替换，不添加，返回结果为目标端替换完毕的结果
    * @function
@@ -1601,7 +1601,10 @@
      * @returns {string}
      */
     this.getShowTextValue = function (menuKey) {
-      return getTargetMenu(menuKey).showText();
+      return getTargetMenu(menuKey).showText(
+        this.getText(menuKey),
+        this.get(menuKey)
+      );
     };
     /**
      * 根据键值获取accessKey值
