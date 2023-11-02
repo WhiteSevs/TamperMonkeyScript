3c1bbbb262f367cd339b226f88cddf14af47acf9
WhiteSev <893177236@qq.com>
2023-11-02 10:43:48 +0800

    更新Utils库版本至2023-11-2;

--- 调试/调试.user.js
+++ 调试/调试.user.js
@@ -35,7 +35,7 @@
 // @grant        GM.ChromeXt
 // @connect      *
 // @run-at       document-start
-// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1271089
+// @require      https://greasyfork.org/scripts/455186-whitesevsutils/code/WhiteSevsUtils.js?version=1273928
 // @require      https://greasyfork.org/scripts/465772-domutils/code/DOMUtils.js?version=1270549
 // ==/UserScript==
 
