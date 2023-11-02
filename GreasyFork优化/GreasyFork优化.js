cf06288591a32dfe064c21b812dc266c29f0e8d3
WhiteSev <893177236@qq.com>
2023-11-03 01:12:52 +0800

    新增菜单功能-美化页面

--- GreasyFork优化/GreasyFork优化.js
+++ GreasyFork优化/GreasyFork优化.js
@@ -2,7 +2,7 @@
 // @name         GreasyFork优化
 // @namespace    https://greasyfork.org/zh-CN/scripts/475722
 // @supportURL   https://greasyfork.org/zh-CN/scripts/475722/feedback
-// @version      2023.10.27
+// @version      2023.11.3
 // @description  自动登录、快捷寻找自己库被其他脚本引用、更新自己的脚本列表、库、优化图片浏览
 // @author       WhiteSevs
 // @license      MIT
@@ -181,6 +181,11 @@
         {
           key: "autoLogin",
           text: "自动登录",
+          enable: true,
+        },
+        {
+          key: "beautifyPage",
+          text: "美化页面",
           enable: true,
         },
       ]);
@@ -525,6 +530,19 @@
         }
         log.success("1秒后点击同步按钮");
         setTimeout(() => {
+          let existUpdate =
+            document.querySelector("input#script_script_sync_type_id_2")
+              .checked ||
+            document.querySelector("input#script_script_sync_type_id_1")
+              .checked;
+          if (!existUpdate) {
+            Qmsg.error("该脚本不存在检查更新并同步");
+            Qmsg.success("下一个: " + nextUrlInfo.nextUrl);
+            setTimeout(() => {
+              window.location.href = nextUrlInfo.nextUrl;
+            }, 1000);
+            return;
+          }
           let btnUpdateAndSync = document.querySelector(
             "input[name='update-and-sync']"
           );
@@ -684,6 +702,10 @@
       log.info(["统计信息", scriptStatsJSON]);
       let todayStatsJSON =
         scriptStatsJSON[utils.formatTime(undefined, "yyyy-MM-dd")];
+      if (!todayStatsJSON) {
+        log.error("今日份的统计信息不存在");
+        return;
+      }
       let update_checks = todayStatsJSON["update_checks"];
       log.info(["今日统计信息", todayStatsJSON]);
       DOMUtils.after(
@@ -701,11 +723,128 @@
         })
       );
     },
+    /**
+     * 优化页面CSS显示，包括markdown
+     */
+    beautifyPage() {
+      let beautifyCSS = `
+      code{font-family:Menlo,Monaco,Consolas,"Courier New",monospace;font-size:.85em;color:#000;background-color:#f0f0f0;border-radius:3px;padding:.2em 0}
+      table{text-indent:initial}
+      table{margin:10px 0 15px 0;border-collapse:collapse;border-spacing:0;display:block;width:100%;overflow:auto;word-break:normal;word-break:keep-all}
+      code,pre{color:#333;background:0 0;font-family:Consolas,"Liberation Mono",Menlo,Courier,monospace;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;word-wrap:normal;line-height:1.4;-moz-tab-size:8;-o-tab-size:8;tab-size:8;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none}
+      pre{padding:.8em;overflow:auto;border-radius:3px;background:#f5f5f5}
+      :not(pre)>code{padding:.1em;border-radius:.3em;white-space:normal;background:#f5f5f5}
+      html body{font-family:"Helvetica Neue",Helvetica,"Segoe UI",Arial,freesans,sans-serif;font-size:16px;line-height:1.6;color:#333;background-color:#fff;overflow:initial;box-sizing:border-box;word-wrap:break-word}
+      html body>:first-child{margin-top:0}
+      html body h1,html body h2,html body h3,html body h4,html body h5,html body h6{line-height:1.2;margin-top:1em;margin-bottom:16px;color:#000}
+      html body h1{font-size:2.25em;font-weight:300;padding-bottom:.3em}
+      html body h2{font-size:1.75em;font-weight:400;padding-bottom:.3em}
+      html body h3{font-size:1.5em;font-weight:500}
+      html body h4{font-size:1.25em;font-weight:600}
+      html body h5{font-size:1.1em;font-weight:600}
+      html body h6{font-size:1em;font-weight:600}
+      html body h1,html body h2,html body h3,html body h4,html body h5{font-weight:600}
+      html body h5{font-size:1em}
+      html body h6{color:#5c5c5c}
+      html body strong{color:#000}
+      html body del{color:#5c5c5c}
+      html body a:not([href]){color:inherit;text-decoration:none}
+      html body a{text-decoration:none}
+      html body a:hover{color:#00a3f5;text-decoration:none}
+      html body img{max-width:100%}
+      html body>p{margin-top:0;margin-bottom:16px;word-wrap:break-word}
+      html body>ol,html body>ul{margin-bottom:16px}
+      html body ol,html body ul{padding-left:2em}
+      html body ol.no-list,html body ul.no-list{padding:0;list-style-type:none}
+      html body ol ol,html body ol ul,html body ul ol,html body ul ul{margin-top:0;margin-bottom:0}
+      html body li{margin-bottom:0}
+      html body li.task-list-item{list-style:none}
+      html body li>p{margin-top:0;margin-bottom:0}
+      html body .task-list-item-checkbox{margin:0 .2em .25em -1.8em;vertical-align:middle}
+      html body .task-list-item-checkbox:hover{cursor:pointer}
+      html body blockquote{margin:16px 0;font-size:inherit;padding:0 15px;color:#5c5c5c;background-color:#f0f0f0;border-left:4px solid #d6d6d6}
+      html body blockquote>:first-child{margin-top:0}
+      html body blockquote>:last-child{margin-bottom:0}
+      html body hr{height:4px;margin:32px 0;background-color:#d6d6d6;border:0 none}
+      html body table{margin:10px 0 15px 0;border-collapse:collapse;border-spacing:0;display:block;width:100%;overflow:auto;word-break:normal;word-break:keep-all}
+      html body table th{font-weight:700;color:#000}
+      html body table td,html body table th{border:1px solid #d6d6d6;padding:6px 13px}
+      html body dl{padding:0}
+      html body dl dt{padding:0;margin-top:16px;font-size:1em;font-style:italic;font-weight:700}
+      html body dl dd{padding:0 16px;margin-bottom:16px}
+      html body code{font-family:Menlo,Monaco,Consolas,"Courier New",monospace;font-size:.85em;color:#000;background-color:#f0f0f0;border-radius:3px;padding:.2em 0}
+      html body code::after,html body code::before{letter-spacing:-.2em;content:"\\00a0"}
+      html body pre>code{padding:0;margin:0;word-break:normal;white-space:pre;background:0 0;border:0}
+      html body .highlight{margin-bottom:16px}
+      html body .highlight pre,html body pre{padding:1em;overflow:auto;line-height:1.45;border:#d6d6d6;border-radius:3px}
+      html body .highlight pre{margin-bottom:0;word-break:normal}
+      html body pre code,html body pre tt{display:inline;max-width:initial;padding:0;margin:0;overflow:initial;line-height:inherit;word-wrap:normal;background-color:transparent;border:0}
+      html body pre code:after,html body pre code:before,html body pre tt:after,html body pre tt:before{content:normal}
+      html body blockquote,html body dl,html body ol,html body p,html body pre,html body ul{margin-top:0;margin-bottom:16px}
+      html body kbd{color:#000;border:1px solid #d6d6d6;border-bottom:2px solid #c7c7c7;padding:2px 4px;background-color:#f0f0f0;border-radius:3px}
+      @media print{html body{background-color:#fff}
+      html body h1,html body h2,html body h3,html body h4,html body h5,html body h6{color:#000;page-break-after:avoid}
+      html body blockquote{color:#5c5c5c}
+      html body pre{page-break-inside:avoid}
+      html body table{display:table}
+      html body img{display:block;max-width:100%;max-height:100%}
+      html body code,html body pre{word-wrap:break-word;white-space:pre}
+      }
+      .scrollbar-style::-webkit-scrollbar{width:8px}
+      .scrollbar-style::-webkit-scrollbar-track{border-radius:10px;background-color:transparent}
+      .scrollbar-style::-webkit-scrollbar-thumb{border-radius:5px;background-color:rgba(150,150,150,.66);border:4px solid rgba(150,150,150,.66);background-clip:content-box}
+      `;
+      /**
+       * 未派上用场的CSS
+       */
+      let notUseBeautifyCSS = `
+      .token.blockquote,.token.comment{color:#969896}
+      .token.cdata{color:#183691}
+      .token.doctype,.token.macro.property,.token.punctuation,.token.variable{color:#333}
+      .token.builtin,.token.important,.token.keyword,.token.operator,.token.rule{color:#a71d5d}
+      .token.attr-value,.token.regex,.token.string,.token.url{color:#183691}
+      .token.atrule,.token.boolean,.token.code,.token.command,.token.constant,.token.entity,.token.number,.token.property,.token.symbol{color:#0086b3}
+      .token.prolog,.token.selector,.token.tag{color:#63a35c}
+      .token.attr-name,.token.class,.token.class-name,.token.function,.token.id,.token.namespace,.token.pseudo-class,.token.pseudo-element,.token.url-reference .token.variable{color:#795da3}
+      .token.entity{cursor:help}
+      .token.title,.token.title .token.punctuation{font-weight:700;color:#1d3e81}
+      .token.list{color:#ed6a43}
+      .token.inserted{background-color:#eaffea;color:#55a532}
+      .token.deleted{background-color:#ffecec;color:#bd2c00}
+      .token.bold{font-weight:700}
+      .token.italic{font-style:italic}
+      .language-json .token.property{color:#183691}
+      .language-markup .token.tag .token.punctuation{color:#333}
+      .language-css .token.function,code.language-css{color:#0086b3}
+      .language-yaml .token.atrule{color:#63a35c}
+      code.language-yaml{color:#183691}
+      .language-ruby .token.function{color:#333}
+      .language-markdown .token.url{color:#795da3}
+      .language-makefile .token.symbol{color:#795da3}
+      .language-makefile .token.variable{color:#183691}
+      .language-makefile .token.builtin{color:#0086b3}
+      .language-bash .token.keyword{color:#0086b3}
+      pre[data-line]{position:relative;padding:1em 0 1em 3em}
+      pre[data-line] .line-highlight-wrapper{position:absolute;top:0;left:0;background-color:transparent;display:block;width:100%}
+      pre[data-line] .line-highlight{position:absolute;left:0;right:0;padding:inherit 0;margin-top:1em;background:hsla(24,20%,50%,.08);background:linear-gradient(to right,hsla(24,20%,50%,.1) 70%,hsla(24,20%,50%,0));pointer-events:none;line-height:inherit;white-space:pre}
+      pre[data-line] .line-highlight:before,pre[data-line] .line-highlight[data-end]:after{content:attr(data-start);position:absolute;top:.4em;left:.6em;min-width:1em;padding:0 .5em;background-color:hsla(24,20%,50%,.4);color:#f4f1ef;font:bold 65%/1.5 sans-serif;text-align:center;vertical-align:.3em;border-radius:999px;text-shadow:none;box-shadow:0 1px #fff}
+      pre[data-line] .line-highlight[data-end]:after{content:attr(data-end);top:auto;bottom:.4em}
+      `;
+      if (GreasyforkMenu.menu.get("beautifyPage")) {
+        GM_addStyle(beautifyCSS);
+      }
+    },
   };
   /* -----------------↑函数区域↑----------------- */
 
+
+
+
+
+
   /* -----------------↓执行入口↓----------------- */
   GreasyforkMenu.init();
+  GreasyforkBusiness.beautifyPage();
   DOMUtils.ready(function () {
     GreasyforkMenu.initEnv();
     if (GreasyforkMenu.menu.get("autoLogin")) {
