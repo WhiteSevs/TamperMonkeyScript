a78bbe2e11a4bffd7824edfc2c14a7fc0150e17a
WhiteSev <893177236@qq.com>
2023-11-03 18:25:05 +0800

    更新版本号

--- 【移动端】-百度系优化/【移动端】-百度系优化.js
+++ 【移动端】-百度系优化/【移动端】-百度系优化.js
@@ -3,7 +3,7 @@
 // @icon         https://www.baidu.com/favicon.ico
 // @namespace    https://greasyfork.org/zh-CN/scripts/418349-移动端-百度系优化
 // @supportURL   https://greasyfork.org/zh-CN/scripts/418349-移动端-百度系优化/feedback
-// @version      2023.11.3
+// @version      2023.11.3.18
 // @author       WhiteSevs
 // @description  用于【移动端】的百度系列产品优化，包括【百度搜索】、【百家号】、【百度贴吧】、【百度文库】、【百度经验】、【百度百科】、【百度知道】、【百度翻译】、【百度图片】、【百度地图】、【百度好看视频】、【百度爱企查】、【百度问题】、【百度识图】等
 // @match        *://m.baidu.com/*
