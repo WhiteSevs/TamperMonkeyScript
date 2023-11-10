8b1c45bc6a2de795cf023d8957453735c01e952d
白柒 <893177236@qq.com>
2023-11-10 12:39:27 +0800

    更新 【移动端】-百度系优化.js
    beta：修复百家号屏蔽精彩推荐失败的问题

--- 【移动端】-百度系优化/【移动端】-百度系优化.js
+++ 【移动端】-百度系优化/【移动端】-百度系优化.js
@@ -3,7 +3,7 @@
 // @icon         https://www.baidu.com/favicon.ico
 // @namespace    https://greasyfork.org/zh-CN/scripts/418349-移动端-百度系优化
 // @supportURL   https://greasyfork.org/zh-CN/scripts/418349-移动端-百度系优化/feedback
-// @version      2023.11.9.19
+// @version      2023.11.10
 // @author       WhiteSevs
 // @description  用于【移动端】的百度系列产品优化，包括【百度搜索】、【百家号】、【百度贴吧】、【百度文库】、【百度经验】、【百度百科】、【百度知道】、【百度翻译】、【百度图片】、【百度地图】、【百度好看视频】、【百度爱企查】、【百度问题】、【百度识图】等
 // @match        *://m.baidu.com/*
@@ -3521,7 +3521,7 @@
         }`);
         /* 简单UA下的精彩推荐 */
         GM_addStyle(`
-        #page_wrapper div.spider > div[class]:nth-child(4){
+        #page_wrapper div.spider > div[class=""]:nth-child(5){
           display: none !important;
         }`);
       }
