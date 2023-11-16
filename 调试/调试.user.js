4a10bce3cede3f34daaf6d1d67e86c65878826bf
白柒 <893177236@qq.com>
2023-11-16 14:51:18 +0800

    修复ChromeXt不存在问题、修改获取方式

--- 调试/调试.user.js
+++ 调试/调试.user.js
@@ -2,7 +2,7 @@
 // @name         调试
 // @namespace    https://greasyfork.org/zh-CN/scripts/475424
 // @supportURL   https://greasyfork.org/zh-CN/scripts/475424/feedback
-// @version      2023.11.15
+// @version      2023.11.16
 // @description  用于调试油猴和相关API
 // @author       WhiteSevs
 // @license      GPL-3.0-only
@@ -41,40 +41,68 @@
 
 (function () {
   let utils = window.Utils.noConflict();
-  unsafeWindow.GM_bridge = {
+  let __unsafeWindow__ =
+    typeof unsafeWindow === "undefined" ? globalThis : unsafeWindow;
+  let exportObj = {
     win: window,
-    unsafeWindow,
     doc: document,
-    globalThis,
-    self,
+    globalThis: globalThis,
+    self: self,
     topWindow: top.window,
     log: new utils.Log(GM_info),
     httpx: new utils.Httpx(GM_xmlhttpRequest),
     Utils: utils,
+    utils: utils,
     DOMUtils: window.DOMUtils,
-    GM_addElement,
-    GM_addStyle,
-    GM_download,
-    GM_getResourceText,
-    GM_getResourceURL,
-    GM_info,
-    GM_log,
-    GM_notification,
-    GM_openInTab,
-    GM_registerMenuCommand,
-    GM_unregisterMenuCommand,
-    GM_setClipboard,
-    GM_getTab,
-    GM_saveTab,
-    GM_getTabs,
-    GM_setValue,
-    GM_getValue,
-    GM_deleteValue,
-    GM_listValues,
-    GM_addValueChangeListener,
-    GM_removeValueChangeListener,
-    GM_xmlhttpRequest,
-    GM_cookie,
-    ChromeXt,
+    unsafeWindow: __unsafeWindow__,
+    GM_addElement:
+      typeof GM_addElement === "undefined" ? undefined : GM_addElement,
+    GM_addStyle: typeof GM_addStyle === "undefined" ? undefined : GM_addStyle,
+    GM_download: typeof GM_download === "undefined" ? undefined : GM_download,
+    GM_getResourceText:
+      typeof GM_getResourceText === "undefined"
+        ? undefined
+        : GM_getResourceText,
+    GM_getResourceURL:
+      typeof GM_getResourceURL === "undefined" ? undefined : GM_getResourceURL,
+    GM_info: typeof GM_info === "undefined" ? undefined : GM_info,
+    GM_log: typeof GM_log === "undefined" ? undefined : GM_log,
+    GM_notification:
+      typeof GM_notification === "undefined" ? undefined : GM_notification,
+    GM_registerMenuCommand:
+      typeof GM_registerMenuCommand === "undefined"
+        ? undefined
+        : GM_registerMenuCommand,
+    GM_unregisterMenuCommand:
+      typeof GM_unregisterMenuCommand === "undefined"
+        ? undefined
+        : GM_unregisterMenuCommand,
+    GM_setClipboard:
+      typeof GM_setClipboard === "undefined" ? undefined : GM_setClipboard,
+    GM_getTab: typeof GM_getTab === "undefined" ? undefined : GM_getTab,
+    GM_saveTab: typeof GM_saveTab === "undefined" ? undefined : GM_saveTab,
+    GM_getTabs: typeof GM_getTabs === "undefined" ? undefined : GM_getTabs,
+    GM_setValue: typeof GM_setValue === "undefined" ? undefined : GM_setValue,
+    GM_getValue: typeof GM_getValue === "undefined" ? undefined : GM_getValue,
+    GM_deleteValue:
+      typeof GM_deleteValue === "undefined" ? undefined : GM_deleteValue,
+    GM_listValues:
+      typeof GM_listValues === "undefined" ? undefined : GM_listValues,
+    GM_addValueChangeListener:
+      typeof GM_addValueChangeListener === "undefined"
+        ? undefined
+        : GM_addValueChangeListener,
+    GM_removeValueChangeListener:
+      typeof GM_removeValueChangeListener === "undefined"
+        ? undefined
+        : GM_removeValueChangeListener,
+    GM_xmlhttpRequest:
+      typeof GM_xmlhttpRequest === "undefined" ? undefined : GM_xmlhttpRequest,
+    GM_cookie: typeof GM_cookie === "undefined" ? undefined : GM_cookie,
+    ChromeXt: typeof ChromeXt === "undefined" ? undefined : ChromeXt,
+    GM_addElement:
+      typeof GM_addElement === "undefined" ? undefined : GM_addElement,
   };
+  exportObj = Object.freeze(exportObj);
+  __unsafeWindow__.GM_bridge = exportObj;
 })();
