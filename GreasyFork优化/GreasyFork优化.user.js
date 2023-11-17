af8a0fb22f632d9053962527cab6f86eaea9bc30
白柒 <893177236@qq.com>
2023-11-17 14:24:42 +0800

    更新 GreasyFork优化.user.js
    调整搜索页面的搜索按钮样式

--- GreasyFork优化/GreasyFork优化.user.js
+++ GreasyFork优化/GreasyFork优化.user.js
@@ -2,7 +2,7 @@
 // @name         GreasyFork优化
 // @namespace    https://greasyfork.org/zh-CN/scripts/475722
 // @supportURL   https://greasyfork.org/zh-CN/scripts/475722/feedback
-// @version      2023.11.15
+// @version      2023.11.17
 // @description  自动登录账号、快捷寻找自己库被其他脚本引用、更新自己的脚本列表、库、优化图片浏览、美化页面
 // @author       WhiteSevs
 // @license      MIT
@@ -1051,7 +1051,12 @@
         border-width: 1px;
         border-radius: 3px;
         margin-right: 0;
-      }`;
+      }
+      input.search-submit{
+        transform: translateY(-5%) !important;
+        margin-left: 10px;
+      }
+      `;
 
       GM_addStyle(compatibleBeautifyCSS);
       if (utils.isPhone()) {
