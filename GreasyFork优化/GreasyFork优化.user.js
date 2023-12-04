cf8c8f44769f8dae5c62c00b3faf2de1a91c0d96
白柒 <893177236@qq.com>
2023-12-04 20:23:48 +0800

    更新Utils库版本

--- GreasyFork优化/GreasyFork优化.user.js
+++ GreasyFork优化/GreasyFork优化.user.js
@@ -21,7 +21,7 @@
 // @require      https://update.greasyfork.org/scripts/449471/1249086/Viewer.js
 // @require      https://update.greasyfork.org/scripts/462234/1284140/Message.js
 // @require      https://update.greasyfork.org/scripts/456485/1285662/pops.js
-// @require      https://update.greasyfork.org/scripts/455186/1285083/WhiteSevsUtils.js
+// @require      https://update.greasyfork.org/scripts/455186/1290431/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1274595/DOMUtils.js
 // ==/UserScript==
 
