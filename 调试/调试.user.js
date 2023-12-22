a267cc826cfc32621e3c5184475f0c5e17e1e445
白柒 <893177236@qq.com>
2023-12-22 14:40:08 +0800

    更新Utils库

--- 调试/调试.user.js
+++ 调试/调试.user.js
@@ -35,7 +35,7 @@
 // @grant        GM.ChromeXt
 // @connect      *
 // @run-at       document-start
-// @require      https://update.greasyfork.org/scripts/455186/1295728/WhiteSevsUtils.js
+// @require      https://update.greasyfork.org/scripts/455186/1299890/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1296917/DOMUtils.js
 // ==/UserScript==
 
