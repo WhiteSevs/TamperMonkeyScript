0dc752ca5534ac0f3e2e3ac80a97936ba5a89176
白柒 <893177236@qq.com>
2023-12-23 18:37:56 +0800

    更新pops库

--- GreasyFork优化/GreasyFork优化.user.js
+++ GreasyFork优化/GreasyFork优化.user.js
@@ -20,7 +20,7 @@
 // @connect      greasyfork.org
 // @require      https://update.greasyfork.org/scripts/449471/1249086/Viewer.js
 // @require      https://update.greasyfork.org/scripts/462234/1284140/Message.js
-// @require      https://update.greasyfork.org/scripts/456485/1300425/pops.js
+// @require      https://update.greasyfork.org/scripts/456485/1300442/pops.js
 // @require      https://update.greasyfork.org/scripts/455186/1299890/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1296917/DOMUtils.js
 // ==/UserScript==
