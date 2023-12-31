7b1bb9cde384413549cbc88b823004a0f3b88f2b
白柒 <893177236@qq.com>
2023-12-31 16:25:02 +0800

    新增百度贴吧设置【强制查看被屏蔽的帖子】

--- 【移动端】-百度系优化/【移动端】-百度系优化.user.js
+++ 【移动端】-百度系优化/【移动端】-百度系优化.user.js
@@ -3,7 +3,7 @@
 // @icon         https://www.baidu.com/favicon.ico
 // @namespace    https://greasyfork.org/zh-CN/scripts/418349
 // @supportURL   https://github.com/WhiteSevs/TamperMonkeyScript/issues
-// @version      2023.12.30
+// @version      2023.12.31
 // @author       WhiteSevs
 // @run-at       document-start
 // @description  用于【移动端】的百度系列产品优化，包括【百度搜索】、【百家号】、【百度贴吧】、【百度文库】、【百度经验】、【百度百科】、【百度知道】、【百度翻译】、【百度图片】、【百度地图】、【百度好看视频】、【百度爱企查】、【百度问题】、【百度识图】等
@@ -3264,7 +3264,7 @@
     },
     /**
      * 百度贴吧
-     * document.querySelector("div.app-view").__vue_
+     * document.querySelector("div.app-view").__vue__
      * + disablePbGuide 是否隐藏顶部导航栏
      * + loading 是否隐藏整个页面的内容（清空）
      * + isVideoThread 该帖子是否是个视频，是的话把帖子变成视频样式
@@ -6481,6 +6481,225 @@
             });
           });
         },
+        /**
+         * 强制查看-贴子不存在或者已被删除
+         */
+        repairErrorThread() {
+          /**
+           * 获取页面信息
+           * @returns {Promise<?{
+           * field: object,
+           * PageData: object,
+           * time: number
+           * }>}
+           */
+          async function getPageInfo() {
+            let getResp = await httpx.get(window.location.href, {
+              headers: {
+                "User-Agent": utils.getRandomPCUA(),
+              },
+            });
+            if (!getResp.status) {
+              return;
+            }
+            log.info(getResp);
+            let pageDOM = DOMUtils.parseHTML(
+              getResp.data.responseText,
+              true,
+              true
+            );
+            let postListFirstElement = pageDOM.querySelector(
+              "#j_p_postlist .l_post"
+            );
+            if (!postListFirstElement) {
+              log.error("未找到#j_p_postlist .l_post元素");
+              Qmsg.error("未找到#j_p_postlist .l_post元素");
+              return;
+            }
+            if (!postListFirstElement.hasAttribute("data-field")) {
+              log.error("未找到 data-field 属性");
+              Qmsg.error("未找到 data-field 属性");
+              return;
+            }
+            let field = utils.toJSON(
+              postListFirstElement.getAttribute("data-field")
+            );
+            let PageData = null;
+            let PageDataScriptString = "";
+            Array.from(pageDOM.querySelectorAll("script")).forEach(
+              (scriptElement) => {
+                if (scriptElement.innerHTML.includes("var PageData")) {
+                  PageDataScriptString = `
+                ${PageDataScriptString}
+
+                ${scriptElement.innerHTML}
+
+                `;
+                }
+              }
+            );
+            if (PageDataScriptString === "") {
+              log.error("未找到 PageData的script标签");
+              Qmsg.error("未找到 PageData的script标签");
+              return;
+            }
+            PageData = new Function(`
+              ${PageDataScriptString}
+
+              return PageData;
+              `)();
+            if (!PageData) {
+              log.error("未找到 PageData");
+              Qmsg.error("未找到 PageData");
+              return;
+            }
+            let time =
+              pageDOM.querySelector(
+                "#j_p_postlist .post-tail-wrap span.tail-info:nth-child(6)"
+              )?.innerText || "";
+            if (utils.isNotNull(time)) {
+              time = utils.formatToTimeStamp(time) / 1000;
+            }
+            return {
+              field: field,
+              PageData: PageData,
+              time: time,
+            };
+          }
+          /**
+           * 获取帖子列表信息
+           * @param {object} field
+           * @param {object} PageData
+           * @param {number} time 帖子时间
+           * @returns
+           */
+          function getPostList(field, PageData, time) {
+            let data = {
+              agree: {
+                agree_num: 0,
+                disagree_num: 0,
+              },
+              author: {
+                /* author.user_id */
+                id: field.author.user_id,
+                /* author.user_name */
+                name: field.author.user_name,
+                /* author.user_nickname */
+                name_show: field.author.user_nickname,
+                /* author.portrait */
+                portrait: field.author.portrait,
+                /* author.user_nickname */
+                show_nickname: field.author.user_nickname,
+                type: 1,
+                userhide: 0,
+              },
+              content: [
+                {
+                  /* content.content */
+                  text: field.content.content,
+                  /* parseInt(content.type) */
+                  type: parseInt(field.content.type),
+                },
+              ],
+              floor: 1,
+              game_info: [null],
+              /* content.post_id */
+              id: parseInt(field.content.post_id),
+              is_bub: 0,
+              is_voice: 0,
+              is_vote: 0,
+              ptype: 0,
+              reply_num: PageData.thread.reply_num,
+              sub_post_number: 0,
+              time: time,
+              title: PageData.thread.title,
+              index: 0,
+            };
+            let firstData = data;
+            let secondData = data;
+            secondData.floor = 3;
+            return [firstData, secondData];
+          }
+          utils.waitNodeWithInterval(".app-view", 10000).then(async () => {
+            await utils.waitPropertyByInterval(
+              () => {
+                return document.querySelector(".app-view").__vue__;
+              },
+              () => {
+                return (
+                  typeof document.querySelector(".app-view").__vue__
+                    .isErrorThread === "boolean"
+                );
+              },
+              undefined,
+              10000
+            );
+            let appViewVue = document.querySelector(".app-view").__vue__;
+            if (!(appViewVue && appViewVue.isErrorThread)) {
+              return;
+            }
+            /* 该帖子不能查看 */
+            log.warn("该帖子不能查看 修复中...");
+            Qmsg.info("该帖子不能查看 修复中...");
+            let pageInfo = await getPageInfo();
+            if (!pageInfo) {
+              return;
+            }
+            log.info(["获取到的页面信息", pageInfo]);
+            let postList = getPostList(
+              pageInfo.field,
+              pageInfo.PageData,
+              pageInfo.time
+            );
+            appViewVue.postList = postList;
+            appViewVue.postAuthorId = postList[0].author.id;
+            appViewVue.thread = {
+              agree: {
+                agree_num: 0,
+                disagree_num: 0,
+              },
+              collect_mark_pid: "0",
+              collect_status: 0,
+              create_time: postList[0].time,
+              id: appViewVue.tid,
+              is_frs_mask: 0,
+              is_share_thread: 0,
+              reply_num: postList[0].reply_num,
+              robot_thread_type: 0,
+              t_share_img: "",
+              thread_type: 0,
+              valid_post_num: 0,
+              works_info: {},
+            };
+            appViewVue.forum = {
+              /* PageData.forum.avatar */
+              avatar: pageInfo.PageData.forum.avatar,
+              /* PageData.forum.first_class */
+              first_dir: pageInfo.PageData.forum.first_class,
+              /* PageData.forum.id */
+              id: pageInfo.PageData.forum.id,
+              is_exists: 1,
+              is_forbidden: 0,
+              is_forum_merged: 0,
+              /* PageData.forum.name */
+              name: pageInfo.PageData.forum.name,
+              /* PageData.forum.second_class */
+              second_dir: pageInfo.PageData.forum.second_class,
+            };
+            /* 固定一下值吧，没测出什么作用 */
+            appViewVue.postNum = 100;
+
+            appViewVue.isErrorThread = false;
+            setTimeout(() => {
+              DOMUtils.append(
+                document.querySelector(
+                  "div.app-view div.thread-main-wrapper .thread-text"
+                ),
+                postList[0].content[0].text
+              );
+            }, 300);
+          });
+        },
       };
 
       if (PopsPanel.getValue("baidu_tieba_clientCallMasquerade")) {
@@ -6500,6 +6719,10 @@
         if (PopsPanel.getValue("baidu_tieba_optimize_image_preview")) {
           log.success("优化图片预览");
           tiebaPost.optimizeImagePreview();
+        }
+        if (PopsPanel.getValue("baidu_tieba_repairErrorThread")) {
+          log.success("强制查看-贴子不存在或者已被删除");
+          tiebaPost.repairErrorThread();
         }
       }
       if (
@@ -8359,6 +8582,18 @@
                   "优化图片点击预览",
                   "baidu_tieba_optimize_image_preview",
                   true
+                ),
+                PopsPanel.getSwtichDetail(
+                  "强制查看被屏蔽的帖子",
+                  "baidu_tieba_repairErrorThread",
+                  false,
+                  function (event, enable) {
+                    if (enable) {
+                      window.alert(
+                        "开启后，如果查看的帖子显示【贴子不存在或者已被删除】，且该帖子在PC端可以查看，那么该修复可以生效。"
+                      );
+                    }
+                  }
                 ),
                 PopsPanel.getSwtichDetail(
                   "点击楼主头像正确跳转主页",
