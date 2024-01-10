f71e670ea98df8092ddbb2dde2012ee89a01ae51
白柒 <893177236@qq.com>
2024-01-10 17:39:44 +0800

    update

--- CSDN简书优化/CSDN简书优化.user.js
+++ CSDN简书优化/CSDN简书优化.user.js
@@ -19,7 +19,7 @@
 // @grant        unsafeWindow
 // @run-at       document-start
 // @require      https://update.greasyfork.org/scripts/449471/1305484/Viewer.js
-// @require      https://update.greasyfork.org/scripts/456485/1308707/pops.js
+// @require      https://update.greasyfork.org/scripts/456485/1309370/pops.js
 // @require      https://update.greasyfork.org/scripts/455186/1307823/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1307605/DOMUtils.js
 // ==/UserScript==
