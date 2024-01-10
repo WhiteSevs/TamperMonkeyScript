3c6fbf782c8acdc1c72bc0bd05f3c29cfcdd1131
白柒 <893177236@qq.com>
2024-01-10 17:35:23 +0800

    更新 pops.d.ts

--- 库.ts/pops.d.ts
+++ 库.ts/pops.d.ts
@@ -1838,3 +1838,201 @@ declare interface PopsRightClickMenuDetails {
      */
     preventDefault?: boolean | undefined;
 }
+
+/**
+ * config.data
+ */
+declare interface PopsSearchSuggestionData {
+    /**
+     * 存储的值
+     */
+    value: any;
+    /**
+     * li元素的html
+     */
+    text: string;
+}
+/**
+ * 搜索建议悬浮窗
+ * pops.searchSuggestion
+ */
+declare interface PopsSearchSuggestionDetails {
+    /**
+     * 目标元素，一般是input
+     */
+    target: HTMLInputElement;
+    /**
+     * 数据
+     */
+    data: PopsSearchSuggestionData[];
+    /**
+     * 右边的删除按钮图标
+     */
+    deleteIcon: {
+        /**
+         * 是否启用，默认true
+         */
+        enable: boolean;
+        /**
+         * 点击回调
+         */
+        callback: (event: Event, liElement: HTMLLIElement, data: PopsSearchSuggestionData) => void;
+    };
+    /**
+     * 自定义的className
+     */
+    className: string;
+    /**
+     * position是否使用absolut，默认true
+     */
+    isAbsolute: boolean;
+    /**
+     * 是否启用动画，默认true
+     */
+    isAnimation: true;
+    /**
+     * 建议框的宽度，默认250px
+     */
+    width: string;
+    /**
+     * 是否和config.target的宽度同步，默认true
+     */
+    followTargetWidth: true;
+    /**
+     * 建议框的最大高度，默认300px
+     */
+    maxHeight: string;
+    /**
+     * 建议框距离元素的距离，默认0
+     */
+    topDistance: number;
+    /**
+     * 层级，默认10000
+     */
+    zIndex: 10000;
+    /**
+     * 搜索中的提示
+     */
+    searchingTip: string;
+    /**
+     * 没有搜索结果的提示的html
+     */
+    toSearhNotResultHTML: string;
+    /**
+     * 获取每一项的html
+     */
+    getItemHTML: (item: PopsSearchSuggestionData) => string;
+    /**
+     * 当config.target触发input时自动调用该函数来获取数据
+     */
+    getData: (value: string) => Promise<any[]>;
+    /**
+     * 每一项的点击回调
+     */
+    itemClickCallBack: (event: Event, liElement: HTMLLIElement, data: PopsSearchSuggestionData) => void;
+}
+
+declare interface PopsSearchSuggestionResult {
+    /**
+     * 根元素
+     */
+    root: HTMLElement;
+    /**
+     * ul元素
+     */
+    hintULElement: HTMLUListElement;
+    /**
+     * 初始化
+     */
+    init: () => void;
+    /**
+     * 获取显示出搜索建议框的html
+     */
+    getSearchSelectElement: () => HTMLElement;
+    /**
+     * 获取显示出搜索建议框的每一项的html
+     */
+    getSearchItemLiElement: (item: PopsSearchSuggestionData, index: number) => HTMLElement;
+    /**
+     * 获取data-value值
+     */
+    getItemDataValue: (item: PopsSearchSuggestionData) => PopsSearchSuggestionData;
+    /**
+     * 设置搜索建议框每一项的点击事件
+     */
+    setSearchItemClickEvent: (liElement: HTMLLIElement) => void;
+    /**
+     * 监听输入框内容改变
+     */
+    setInputChangeEvent: () => void;
+    /**
+     * 移除输入框内容改变的监听
+     */
+    removeInputChangeEvent: () => void;
+    /**
+     * 设置显示搜索建议框的事件
+     */
+    setShowEvent: () => void;
+    /**
+     * 移除显示搜索建议框的事件
+     */
+    removeShowEvent: () => void;
+    /**
+     * 设置隐藏搜索建议框的事件
+     */
+    setHideEvent: () => void;
+    /**
+     * 移除隐藏搜索建议框的事件
+     */
+    removeHideEvent: () => void;
+    /**
+     * 设置所有监听
+     */
+    setAllEvent: () => void;
+    /**
+     * 移除所有监听
+     */
+    removeAllEvent: () => void;
+    /**
+     * 获取删除按钮的html
+     */
+    getDeleteIconHTML: (size: number, fill: string) => void;
+    /**
+     * 设置当前正在搜索中的提示
+     */
+    setPromptsInSearch: () => void;
+    /**
+     * 移除正在搜索中的提示
+     */
+    removePromptsInSearch: () => void;
+    /**
+     * 清空所有的搜索结果
+     */
+    clearAllSearchItemLi: () => void;
+    /**
+     * 修改搜索建议框的位置(top、left)
+     * 因为目标元素可能是动态隐藏的
+     */
+    changeHintULElementPosition: () => void;
+    /**
+     * 修改搜索建议框的width
+     * 因为目标元素可能是动态隐藏的
+     */
+    changeHintULElementWidth: () => void;
+    /**
+     * 更新页面显示的搜索结果
+     */
+    update: (data: PopsSearchSuggestionData) => void;
+    /**
+     * 清空当前的搜索结果并显示无结果
+     */
+    clear: () => void;
+    /**
+     * 隐藏搜索建议框
+     */
+    hide: () => void;
+    /**
+     * 显示搜索建议框
+     */
+    show: () => void;
+}
