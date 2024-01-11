4e4729192494f2ffdffa2767c503288087cd072a
白柒 <893177236@qq.com>
2024-01-11 13:56:33 +0800

    重命名utils.enterFullscreen=>utils.enterFullScreen

--- 【移动端】-百度系优化/【移动端】-百度系优化.user.js
+++ 【移动端】-百度系优化/【移动端】-百度系优化.user.js
@@ -3,7 +3,7 @@
 // @icon         https://www.baidu.com/favicon.ico
 // @namespace    https://greasyfork.org/zh-CN/scripts/418349
 // @supportURL   https://github.com/WhiteSevs/TamperMonkeyScript/issues
-// @version      2024.1.10
+// @version      2024.1.11
 // @author       WhiteSevs
 // @run-at       document-start
 // @description  用于【移动端】的百度系列产品优化，包括【百度搜索】、【百家号】、【百度贴吧】、【百度文库】、【百度经验】、【百度百科】、【百度知道】、【百度翻译】、【百度图片】、【百度地图】、【百度好看视频】、【百度爱企查】、【百度问题】、【百度识图】等
@@ -53,8 +53,8 @@
 // @require      https://update.greasyfork.org/scripts/449471/1305484/Viewer.js
 // @require      https://update.greasyfork.org/scripts/462234/1307862/Message.js
 // @require      https://update.greasyfork.org/scripts/456485/1309370/pops.js
-// @require      https://update.greasyfork.org/scripts/455186/1307823/WhiteSevsUtils.js
-// @require      https://update.greasyfork.org/scripts/465772/1307605/DOMUtils.js
+// @require      https://update.greasyfork.org/scripts/455186/1309760/WhiteSevsUtils.js
+// @require      https://update.greasyfork.org/scripts/465772/1309759/DOMUtils.js
 // ==/UserScript==
 
 (function () {
@@ -6606,7 +6606,7 @@
                 let videoElement = document.querySelector(
                   "#video video.hplayer-video"
                 );
-                utils.enterFullscreen(videoElement);
+                utils.enterFullScreen(videoElement);
               }
             }
           }, 0);
