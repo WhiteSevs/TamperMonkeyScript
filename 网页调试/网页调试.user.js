4d0661b30c5bb9b7f2b886da5b96e1160ccb2a1e
白柒 <893177236@qq.com>
2024-01-28 19:46:56 +0800

    更新PageSpy版本至1.7.1；更新WhiteSevsUtils库；

--- 网页调试/网页调试.user.js
+++ 网页调试/网页调试.user.js
@@ -32,8 +32,8 @@
 // @require         https://update.greasyfork.org/scripts/456485/1315529/pops.js
 // @require         https://update.greasyfork.org/scripts/483694/1309598/Eruda-2.js
 // @require         https://update.greasyfork.org/scripts/483695/1309599/vConsole-2.js
-// @require         https://update.greasyfork.org/scripts/483696/1315478/PageSpy-2.js
-// @require         https://update.greasyfork.org/scripts/455186/1317196/WhiteSevsUtils.js
+// @require         https://update.greasyfork.org/scripts/483696/1318687/PageSpy-2.js
+// @require         https://update.greasyfork.org/scripts/455186/1318703/WhiteSevsUtils.js
 // ==/UserScript==
 
 (function () {
