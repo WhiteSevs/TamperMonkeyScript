2861e9433457a446d0f96522155288283f53e027
白柒 <893177236@qq.com>
2024-01-31 20:59:20 +0800

    更新WhiteSevsUtils库

--- CSDN优化/CSDN优化.user.js
+++ CSDN优化/CSDN优化.user.js
@@ -17,7 +17,7 @@
 // @grant        unsafeWindow
 // @run-at       document-start
 // @require      https://update.greasyfork.org/scripts/456485/1315529/pops.js
-// @require      https://update.greasyfork.org/scripts/455186/1319951/WhiteSevsUtils.js
+// @require      https://update.greasyfork.org/scripts/455186/1320376/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1318702/DOMUtils.js
 // ==/UserScript==
 
