d6b645ee9ad8d82dc625310f1e33b83397dfd1c6
白柒 <893177236@qq.com>
2024-02-21 23:42:52 +0800

    君不见，黄河之水天上来，pops库更新不复回

--- 【移动端】百度系优化/【移动端】百度系优化.user.js
+++ 【移动端】百度系优化/【移动端】百度系优化.user.js
@@ -26,7 +26,7 @@
 // @grant        unsafeWindow
 // @require      https://update.greasyfork.org/scripts/449471/1305484/Viewer.js
 // @require      https://update.greasyfork.org/scripts/462234/1322684/Message.js
-// @require      https://update.greasyfork.org/scripts/456485/1330937/pops.js
+// @require      https://update.greasyfork.org/scripts/456485/1331238/pops.js
 // @require      https://update.greasyfork.org/scripts/455186/1329875/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1327726/DOMUtils.js
 // ==/UserScript==
