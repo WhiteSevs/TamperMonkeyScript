7e8cafe52fbcc0230abead61697c9f304212b86e
白柒 <893177236@qq.com>
2024-03-12 23:31:58 +0800

    更新库

--- 调试/调试.user.js
+++ 调试/调试.user.js
@@ -2,7 +2,7 @@
 // @name         调试
 // @namespace    https://greasyfork.org/zh-CN/scripts/475424
 // @supportURL   https://github.com/WhiteSevs/TamperMonkeyScript/issues
-// @version      2024.3.11.23
+// @version      2024.3.12
 // @description  用于调试油猴、ChromeXt和相关API
 // @author       WhiteSevs
 // @license      GPL-3.0-only
@@ -35,8 +35,8 @@
 // @grant        GM.ChromeXt
 // @connect      *
 // @run-at       document-start
-// @require      https://update.greasyfork.org/scripts/455186/1341325/WhiteSevsUtils.js
-// @require      https://update.greasyfork.org/scripts/465772/1341174/DOMUtils.js
+// @require      https://update.greasyfork.org/scripts/455186/1341797/WhiteSevsUtils.js
+// @require      https://update.greasyfork.org/scripts/465772/1341795/DOMUtils.js
 // ==/UserScript==
 
 (function () {
