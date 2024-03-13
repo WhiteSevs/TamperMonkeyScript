f84da9ebf08ce52e326652a001eadcc9982e70c7
白柒 <893177236@qq.com>
2024-03-13 19:40:34 +0800

    更新Utils库

--- CSDN优化/CSDN优化.user.js
+++ CSDN优化/CSDN优化.user.js
@@ -3,7 +3,7 @@
 // @icon         https://www.csdn.net/favicon.ico
 // @namespace    https://greasyfork.org/zh-CN/scripts/406136
 // @supportURL   https://github.com/WhiteSevs/TamperMonkeyScript/issues
-// @version      2024.3.13
+// @version      2024.3.13.19
 // @license      MIT
 // @description  支持手机端和PC端，屏蔽广告，优化浏览体验，自动跳转拦截的URL
 // @author       WhiteSevs
@@ -17,7 +17,7 @@
 // @grant        unsafeWindow
 // @run-at       document-start
 // @require      https://update.greasyfork.org/scripts/456485/1342149/pops.js
-// @require      https://update.greasyfork.org/scripts/455186/1341797/WhiteSevsUtils.js
+// @require      https://update.greasyfork.org/scripts/455186/1342261/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1342148/DOMUtils.js
 // ==/UserScript==
 
