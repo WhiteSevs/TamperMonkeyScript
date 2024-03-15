2bb15331e39cac8b81f0080983e3fc418065b664
白柒 <893177236@qq.com>
2024-03-15 14:10:53 +0800

    更新 网盘链接识别.d.ts

--- 网盘链接识别/网盘链接识别.d.ts
+++ 网盘链接识别/网盘链接识别.d.ts
@@ -100,7 +100,28 @@ declare interface NetiDiskHandleObject {
      */
     matchText: string;
 }
-
+declare interface NetDiskHistoryDataOption {
+    /** 匹配的网站URL */
+    url: string;
+    /** 匹配的网站顶部URL（防止是在iframe内匹配到的） */
+    topURL: string;
+    /** 规则名 */
+    netDiskName: string;
+    /** 规则下标 */
+    netDiskIndex: number;
+    /** 分享码 */
+    shareCode: string;
+    /** 访问码 */
+    accessCode: string;
+    /** 添加的时间 */
+    addTime: number;
+    /** 更新的时间 */
+    updateTime: number;
+    /** 匹配的网址的标题 */
+    title: string;
+    /** 匹配到的文本(2024.3.15版本新增) */
+    matchText: string;
+}
 /** worker的传递的数据 */
 declare interface NetDiskWorkerOptions {
     /**
