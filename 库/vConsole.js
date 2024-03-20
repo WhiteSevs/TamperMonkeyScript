bc923b4fbe8c4ebdea3e6f6d0df66061365d13fb
白柒 <893177236@qq.com>
2024-03-20 17:17:13 +0800

    修复System面板无输出的问题

--- 库/vConsole.js
+++ 库/vConsole.js
@@ -18,8 +18,8 @@ let initVConsole = function (
    * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
    */
   !(function (global, factory) {
-    global[vConsoleName] = factory(global);
-  })(currentWindow, function (window) {
+    global[vConsoleName] = factory(global, global.console);
+  })(currentWindow, function (window, console) {
     return (function () {
       var __webpack_modules__ = {
           4264: function (t, n, e) {
