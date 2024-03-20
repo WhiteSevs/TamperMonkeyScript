d9bb940f01e3971e2b5cda674aa925aa8d81792a
白柒 <893177236@qq.com>
2024-03-20 16:39:50 +0800

    更新pops库

--- 简书优化/简书优化.user.js
+++ 简书优化/简书优化.user.js
@@ -3,7 +3,7 @@
 // @icon         https://www.jianshu.com/favicon.ico
 // @namespace    https://greasyfork.org/zh-CN/scripts/485483
 // @supportURL   https://github.com/WhiteSevs/TamperMonkeyScript/issues
-// @version      2024.3.20.16
+// @version      2024.3.20.16.39
 // @license      MIT
 // @description  支持手机端和PC端，屏蔽广告，优化浏览体验，自动跳转拦截的URL
 // @author       WhiteSevs
@@ -17,7 +17,7 @@
 // @grant        GM_info
 // @grant        unsafeWindow
 // @run-at       document-start
-// @require      https://update.greasyfork.org/scripts/456485/1345972/pops.js
+// @require      https://update.greasyfork.org/scripts/456485/1346051/pops.js
 // @require      https://update.greasyfork.org/scripts/455186/1346030/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1344519/DOMUtils.js
 // ==/UserScript==
