7e02cfd3462507f6acb958009a2d5715df91bb56
白柒 <893177236@qq.com>
2024-03-23 14:46:36 +0800

    update

--- 【移动端】微博优化/【移动端】微博优化.user.js
+++ 【移动端】微博优化/【移动端】微博优化.user.js
@@ -3,7 +3,7 @@
 // @icon         https://favicon.yandex.net/favicon/v2/https://m.weibo.cn/?size=32
 // @namespace    https://greasyfork.org/zh-CN/scripts/480094
 // @supportURL   https://github.com/WhiteSevs/TamperMonkeyScript/issues
-// @version      2024.3.22
+// @version      2024.3.23.14
 // @description  劫持自动跳转登录，修复用户主页正确跳转，伪装客户端，可查看名人堂日程表
 // @author       WhiteSevs
 // @license      MIT
@@ -23,7 +23,7 @@
 // @connect      m.weibo.cn
 // @require      https://update.greasyfork.org/scripts/462234/1322684/Message.js
 // @require      https://update.greasyfork.org/scripts/456485/1347250/pops.js
-// @require      https://update.greasyfork.org/scripts/455186/1346755/WhiteSevsUtils.js
+// @require      https://update.greasyfork.org/scripts/455186/1347753/WhiteSevsUtils.js
 // ==/UserScript==
 
 (function () {
