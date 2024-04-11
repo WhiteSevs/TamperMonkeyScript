4005166c6d4e63064dca1700b30a6956c0df0875
白柒 <893177236@qq.com>
2024-04-11 19:00:51 +0800

    更新pops库

--- 【移动端】百度系优化/【移动端】百度系优化.user.js
+++ 【移动端】百度系优化/【移动端】百度系优化.user.js
@@ -3,7 +3,7 @@
 // @icon         https://www.baidu.com/favicon.ico
 // @namespace    https://greasyfork.org/zh-CN/scripts/418349
 // @supportURL   https://github.com/WhiteSevs/TamperMonkeyScript/issues
-// @version      2024.4.7.20
+// @version      2024.4.11
 // @author       WhiteSevs
 // @run-at       document-start
 // @description  用于【移动端】的百度系列产品优化，包括【百度搜索】、【百家号】、【百度贴吧】、【百度文库】、【百度经验】、【百度百科】、【百度知道】、【百度翻译】、【百度图片】、【百度地图】、【百度好看视频】、【百度爱企查】、【百度问题】、【百度识图】等
@@ -28,7 +28,7 @@
 // @grant        unsafeWindow
 // @require      https://update.greasyfork.org/scripts/449471/1305484/Viewer.js
 // @require      https://update.greasyfork.org/scripts/462234/1322684/Message.js
-// @require      https://update.greasyfork.org/scripts/456485/1352602/pops.js
+// @require      https://update.greasyfork.org/scripts/456485/1357649/pops.js
 // @require      https://update.greasyfork.org/scripts/455186/1355010/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1344519/DOMUtils.js
 // @require      https://update.greasyfork.org/scripts/488179/1332779/showdown.js
