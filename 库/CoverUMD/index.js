7fca0f4181e1c15d2087092e46f974f893e7e054
白柒 <893177236@qq.com>
2024-05-06 00:11:43 +0800

    🦄 refactor(对局部变量进行try-catch):

--- 库/CoverUMD/index.js
+++ 库/CoverUMD/index.js
@@ -1,3 +1,15 @@
-const exports = null;
-const module = null;
-const define = null;
+try {
+  const exports = void 0;
+} catch (error) {
+  console.warn(error);
+}
+try {
+  const module = void 0;
+} catch (error) {
+  console.warn(error);
+}
+try {
+  const define = void 0;
+} catch (error) {
+  console.warn(error);
+}
