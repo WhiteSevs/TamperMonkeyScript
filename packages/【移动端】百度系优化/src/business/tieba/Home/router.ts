c3768e946a7eefd64d1f2da1fbe6458f238ef98e
白柒 <893177236@qq.com>
2024-05-10 12:36:52 +0800

    🐞 fix(修复vue-router的引用):
    * 新增某些情况下获取不到un的报错提示

--- packages/【移动端】百度系优化/src/business/tieba/Home/router.ts
+++ packages/【移动端】百度系优化/src/business/tieba/Home/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router/dist/vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./Home.vue";
 import About from "./About.vue";
 import Follow from "./Follow.vue";
@@ -8,10 +8,29 @@ const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
     history: createWebHashHistory(),
     routes: [
-        { path: '/', component: Home },
-        { path: "/about", component: About },
-        { path: '/follow', component: Follow },
-        { path: '/fans', component: Fans, },
+        {
+            path: '/',
+            name: "Home",
+            component: Home,
+            meta: {
+                keepAlive: true
+            },
+        },
+        {
+            path: "/about",
+            name: "About",
+            component: About
+        },
+        {
+            path: '/follow',
+            name: "Follow",
+            component: Follow
+        },
+        {
+            path: '/fans',
+            name: "Fans",
+            component: Fans,
+        },
     ],
 })
 
