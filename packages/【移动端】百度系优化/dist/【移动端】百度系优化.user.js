d370332d5656932dd901aa351cefc7578fc007db
白柒 <893177236@qq.com>
2024-05-17 15:59:18 +0800

    🔧 build(update Element-Plus require url):

--- packages/【移动端】百度系优化/dist/【移动端】百度系优化.user.js
+++ packages/【移动端】百度系优化/dist/【移动端】百度系优化.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【移动端】百度系优化
 // @namespace    https://github.com/WhiteSevs/TamperMonkeyScript
-// @version      2024.5.14.15
+// @version      2024.5.17
 // @author       WhiteSevs
 // @description  用于【移动端】的百度系列产品优化，包括【百度搜索】、【百家号】、【百度贴吧】、【百度文库】、【百度经验】、【百度百科】、【百度知道】、【百度翻译】、【百度图片】、【百度地图】、【百度好看视频】、【百度爱企查】、【百度问题】、【百度识图】等
 // @license      GPL-3.0-only
@@ -14,17 +14,13 @@
 // @require      https://update.greasyfork.org/scripts/449471/1360565/Viewer.js
 // @require      https://update.greasyfork.org/scripts/462234/1322684/Message.js
 // @require      https://update.greasyfork.org/scripts/456485/1371568/pops.js
-// @require      https://update.greasyfork.org/scripts/455186/1375312/WhiteSevsUtils.js
+// @require      https://update.greasyfork.org/scripts/455186/1377415/WhiteSevsUtils.js
 // @require      https://update.greasyfork.org/scripts/465772/1360574/DOMUtils.js
 // @require      https://update.greasyfork.org/scripts/488179/1360581/showdown.js
 // @require      https://cdn.jsdelivr.net/npm/vue@3.4.27/dist/vue.global.prod.js
-// @require      data:application/javascript,window.Vue%3DVue%3B
 // @require      https://cdn.jsdelivr.net/npm/vue-router@4.3.2/dist/vue-router.global.js
-// @require      data:application/javascript,window.VueRouter%3DVueRouter%3B
-// @require      https://cdn.jsdelivr.net/npm/element-plus@2.7.2/dist/index.full.min.js
-// @require      data:application/javascript,window.ElementPlus%3DElementPlus%3B
+// @require      https://update.greasyfork.org/scripts/495227/1378053/Element-Plus.js
 // @require      https://cdn.jsdelivr.net/npm/@element-plus/icons-vue@2.3.1/dist/index.iife.min.js
-// @require      data:application/javascript,window.ElementPlusIconsVue%3DElementPlusIconsVue%3B
 // @resource     ElementPlusResourceCSS  https://cdn.jsdelivr.net/npm/element-plus@2.7.2/dist/index.min.css
 // @connect      www.baidu.com
 // @connect      m.baidu.com
