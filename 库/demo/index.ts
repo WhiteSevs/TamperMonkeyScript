7175412295e033880ac4e22f4156288dac6d459d
白柒 <893177236@qq.com>
2024-05-23 17:52:26 +0800

    🦄 refactor(替换webpack => rollup):

---
+++ 库/demo/index.ts
@@ -0,0 +1,3 @@
+import { Demo } from "./src/demo";
+
+export { Demo };
---
+++ 库/demo/rollup.config.js
@@ -0,0 +1,28 @@
+// 允许使用 node 或 umd 包
+const commonjs = require("@rollup/plugin-commonjs");
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+// 编译 TS 代码
+const typescript = require("@rollup/plugin-typescript");
+
+module.exports = {
+	plugins: [nodeResolve(), commonjs(), typescript()],
+	input: "./index.ts", // 源文件入口
+	output: [
+		{
+			file: "dist/index.esm.js", // package.json 中 "module": "dist/index.esm.js"
+			format: "esm", // es module 形式的包， 用来import 导入， 可以tree shaking
+			sourcemap: true,
+		},
+		{
+			file: "dist/index.cjs.js", // package.json 中 "main": "dist/index.cjs.js",
+			format: "cjs", // commonjs 形式的包， require 导入
+			sourcemap: true,
+		},
+		{
+			file: "dist/index.umd.js",
+			name: "demo", // 模块名
+			format: "umd", // umd 兼容形式的包， 可以直接应用于网页 script
+			sourcemap: true,
+		},
+	],
+};
