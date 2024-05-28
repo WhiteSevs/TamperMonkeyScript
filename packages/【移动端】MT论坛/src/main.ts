2d72725d629870cbe272db360b19dc4d272671ce
白柒 <893177236@qq.com>
2024-05-28 16:27:52 +0800

    drop

--- packages/【移动端】MT论坛/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp, onBeforeMount, onMounted } from 'vue';
-import { GM_addElement, monkeyWindow, unsafeWindow } from 'ViteGM';
-import { MTElement } from './api/MTElement';
-import { DOMUtils, pops } from '@/env';
-
-
-DOMUtils.ready(() => {
-    console.log("dom is loaded");
-})
--- packages/【移动端】MT论坛/vite.build.ts
+++
@@ -1,5 +0,0 @@
-const SCRIPT_NAME = "【移动端】MT论坛";
-
-export {
-    SCRIPT_NAME,
-}
