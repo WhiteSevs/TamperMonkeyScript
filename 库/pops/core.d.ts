55d337366e0cf7f5961a98f991250fc5bce6f60f
白柒 <893177236@qq.com>
2024-06-08 20:28:23 +0800

    📃 docs(update):

--- 库/pops/core.d.ts
+++ 库/pops/core.d.ts
@@ -399,6 +399,44 @@ declare interface PopsButtonDetails {
 }
 
 /**
+ * 按钮配置
+ */
+declare interface PopsButtonDetailsAnyType {
+	/**
+	 * 是否启用按钮
+	 */
+	enable: boolean;
+	/**
+	 * 图标按钮，如果名字为内置的，则使用内置的，否则为自定义的svg
+	 */
+	icon: PopsIcon;
+	/**
+	 * 图标按钮是否放在右边
+	 */
+	rightIcon: boolean;
+	/**
+	 * 图标按钮是否是旋转360°，默认false
+	 */
+	iconIsLoading: boolean;
+	/**
+	 * 按钮尺寸大小，默认为空
+	 */
+	size: PopsButtonSize;
+	/**
+	 * 按钮样式类型
+	 */
+	type: string;
+	/**
+	 * 按钮文字，默认为空
+	 */
+	text: string;
+	/**
+	 * 按钮点击的回调
+	 */
+	callback(event: PopsBtnCallBackEvent): void;
+}
+
+/**
  * 顶部关闭按钮配置
  */
 declare interface PopsHeaderCloseButtonDetails {
@@ -1054,15 +1092,15 @@ declare interface PopsMoreButtonConfig {
 		/**
 		 * 确定按钮
 		 */
-		ok?: Partial<PopsButtonDetails>;
+		ok?: Partial<PopsButtonDetails | PopsButtonDetailsAnyType>;
 		/**
 		 * 取消按钮
 		 */
-		cancel?: Partial<PopsButtonDetails>;
+		cancel?: Partial<PopsButtonDetails | PopsButtonDetailsAnyType>;
 		/**
 		 * 其他按钮
 		 */
-		other?: Partial<PopsButtonDetails>;
+		other?: Partial<PopsButtonDetails | PopsButtonDetailsAnyType>;
 	};
 }
 /**
@@ -1175,7 +1213,7 @@ declare interface PopsAlertDetails
 		/**
 		 * 确定按钮
 		 */
-		ok?: Partial<PopsButtonDetails>;
+		ok?: Partial<PopsButtonDetails | PopsButtonDetailsAnyType>;
 	};
 }
 
@@ -1436,7 +1474,7 @@ declare interface PopsDrawerDetails
 	/**
 	 * 标题
 	 */
-	title: PopsTitleConfig & {
+	title?: Partial<PopsTitleConfig["title"]> & {
 		/**
 		 * 是否启用，默认true
 		 */
@@ -1445,31 +1483,31 @@ declare interface PopsDrawerDetails
 	/**
 	 * 打开的方向，默认为right
 	 */
-	direction: "top" | "bottom" | "left" | "right";
+	direction?: "top" | "bottom" | "left" | "right";
 	/**
 	 * 窗体的大小; 当使用 number 类型时; 以像素为单位，默认为30%
 	 */
-	size: string | number;
+	size?: string | number;
 	/**
 	 * 是否在 Drawer 出现时将 body 滚动锁定，默认为false
 	 */
-	lockScroll: boolean;
+	lockScroll?: boolean;
 	/**
 	 * 是否可以通过按下 ESC 关闭 Drawer，默认为true
 	 */
-	closeOnPressEscape: boolean;
+	closeOnPressEscape?: boolean;
 	/**
 	 * 打开的延时时间，单位毫秒，默认为0
 	 */
-	openDelay: number;
+	openDelay?: number;
 	/**
 	 * 关闭的延时时间，单位毫秒，默认为0
 	 */
-	closeDelay: number;
+	closeDelay?: number;
 	/**
 	 * border-radius，根据direction自动适应，默认为5
 	 */
-	borderRadius: number;
+	borderRadius?: number;
 }
 /**
  * pops.folder
