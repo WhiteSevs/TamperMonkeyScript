d4821e0860341dc4705e39366337917bcf95c009
白柒 <893177236@qq.com>
2024-06-19 11:50:09 +0800

    #

---
+++ packages/抖音优化/src/main/video/DouYinVideoShortCut.ts
@@ -0,0 +1,56 @@
+import { unsafeWindow } from "ViteGM";
+import { log } from "@/env";
+import { ShortCut, ShortCutMap } from "@/utils/ShortCut";
+import { DouYinVideo, VideoRate } from "./DouYinVideo";
+
+export const DouYinVideoShortcut = {
+	shortCut: new ShortCut("video-short-cut"),
+	$data: {
+		rateMap: ["0.75", "1", "1.25", "1.5", "1.75", "2", "3"] as VideoRate[],
+	},
+	init() {
+		this.shortCut.initGlobalKeyboardListener(this.getShortCutMap());
+	},
+	getShortCutMap(): ShortCutMap {
+		return {
+			"dy-video-rate-low": {
+				callback() {
+					log.info("调用倍速 => 小");
+					let currentRate =
+						unsafeWindow.sessionStorage.getItem("player_playbackratio") ?? "1";
+					let findIndex = DouYinVideoShortcut.$data.rateMap.findIndex(
+						(rate) => {
+							return rate === currentRate;
+						}
+					);
+					if (findIndex === 0) {
+						log.warn("已是最小倍速: " + currentRate);
+						return;
+					}
+					let prevRate = DouYinVideoShortcut.$data.rateMap[findIndex - 1];
+					log.info("设置倍速: " + prevRate);
+					DouYinVideo.chooseVideoRate(prevRate);
+				},
+			},
+			"dy-video-rate-up": {
+				callback() {
+					log.info("调用倍速 => 大");
+					let currentRate =
+						unsafeWindow.sessionStorage.getItem("player_playbackratio") ?? "1";
+					let findIndex = DouYinVideoShortcut.$data.rateMap.findIndex(
+						(rate) => {
+							return rate === currentRate;
+						}
+					);
+					if (findIndex === DouYinVideoShortcut.$data.rateMap.length - 1) {
+						log.warn("已是最大倍速: " + currentRate);
+						return;
+					}
+					let nextRate = DouYinVideoShortcut.$data.rateMap[findIndex + 1];
+					log.info("设置倍速: " + nextRate);
+					DouYinVideo.chooseVideoRate(nextRate);
+				},
+			},
+		};
+	},
+};
