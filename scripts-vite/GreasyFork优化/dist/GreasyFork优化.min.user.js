b0be3078be34596f1aff3ec59365f5ce933c42fd
白柒 <893177236@qq.com>
2024-07-28 23:27:55 +0800

    fix: 修复顶部导航栏【退出】按钮的样式

--- scripts-vite/GreasyFork优化/dist/GreasyFork优化.min.user.js
+++ scripts-vite/GreasyFork优化/dist/GreasyFork优化.min.user.js
@@ -2,7 +2,7 @@
 // @name               GreasyFork优化
 // @name:en-US         GreasyFork Optimization
 // @namespace          https://github.com/WhiteSevs/TamperMonkeyScript
-// @version            2024.7.24
+// @version            2024.7.28
 // @author             WhiteSevs
 // @description        自动登录账号、快捷寻找自己库被其他脚本引用、更新自己的脚本列表、库、优化图片浏览、美化页面、Markdown复制按钮
 // @description:en-US  Automatically log in to the account, quickly find your own library referenced by other scripts, update your own script list, library, optimize image browsing, beautify the page, Markdown copy button
@@ -1022,7 +1022,8 @@ input.search-submit {\r
 	}\r
 \r
 	header#main-header #nav-user-info > span {\r
-		flex: 1;\r
+		/*flex: 1;*/\r
+		flex: 1 0 auto;\r
 	}\r
 \r
 	header#main-header #nav-user-info,\r
