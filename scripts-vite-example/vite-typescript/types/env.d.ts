bef94ccbd79d19e5473d5bc95633578a359627ee
白柒 <893177236@qq.com>
2024-09-17 19:47:47 +0800

    feat: 更新模板

--- scripts-vite-example/vite-typescript/types/env.d.ts
+++ scripts-vite-example/vite-typescript/types/env.d.ts
@@ -11,4 +11,14 @@ type NestedObjectWithToString = {
 	toString(): any;
 };
 
+/**
+ * Promise的兼容类型
+ * 
+ * 它是返回提供的类型或Promise包裹的类型
+ */
 declare type IPromise<T> = Promise<T> | T;
+
+/**
+ * 提取数组中的元素类型
+ */
+declare type ExtractElementType<T> = T extends Array<infer U> ? U : never;
