37843933dbb65c02dea3c150aad852c3c8a24ff7
白柒 <893177236@qq.com>
2025-01-16 21:26:26 +0800

    🐞 fix: 修复部分直播间下内容看不见

--- scripts-vite/抖音优化/src/main/live/DouYinLiveBlock.ts
+++ scripts-vite/抖音优化/src/main/live/DouYinLiveBlock.ts
@@ -123,8 +123,8 @@ export const DouYinLiveBlock = {
 		log.info("【屏蔽】底部的礼物栏");
 		return [
 			CommonUtil.addBlockCSS(
-				// 2024.12.26
-				'div[data-e2e="living-container"] >div> div:not(:has(video)):has(pace-island >.gitBarOptimizeEnabled)',
+				// 2025.1.16
+				'div[data-e2e="living-container"] >div> div:not(:has(video)):not(:has([id^="living_player_container"])):has(pace-island >.gitBarOptimizeEnabled [data-e2e="gifts-container"])',
 				// Firefox上的CSS，多了个pace-island
 				'div[data-e2e="living-container"] >div> div:has(>pace-island >.gitBarOptimizeEnabled)',
 				// 全屏状态下的
