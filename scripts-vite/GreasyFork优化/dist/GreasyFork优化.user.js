8c9bd838596d3f75a3de4231e55d387a0a1809fb
白柒 <893177236@qq.com>
2025-02-09 11:54:26 +0800

    ✨ feat: 适配域名cn-greasyfork.org

--- scripts-vite/GreasyFork优化/dist/GreasyFork优化.user.js
+++ scripts-vite/GreasyFork优化/dist/GreasyFork优化.user.js
@@ -2,7 +2,7 @@
 // @name               GreasyFork优化
 // @name:en-US         GreasyFork Optimization
 // @namespace          https://github.com/WhiteSevs/TamperMonkeyScript
-// @version            2025.2.8
+// @version            2025.2.9
 // @author             WhiteSevs
 // @description        自动登录账号、快捷寻找自己库被其他脚本引用、更新自己的脚本列表、库、优化图片浏览、美化页面、Markdown复制按钮
 // @description:en-US  Automatically log in to the account, quickly find your own library referenced by other scripts, update your own script list, library, optimize image browsing, beautify the page, Markdown copy button
@@ -11,6 +11,7 @@
 // @supportURL         https://github.com/WhiteSevs/TamperMonkeyScript/issues
 // @match              *://greasyfork.org/*
 // @match              *://sleazyfork.org/*
+// @match              *://cn-greasyfork.org/*
 // @require            https://update.greasyfork.org/scripts/494167/1413255/CoverUMD.js
 // @require            https://fastly.jsdelivr.net/npm/@whitesev/utils@2.6.1/dist/index.umd.js
 // @require            https://fastly.jsdelivr.net/npm/@whitesev/domutils@1.4.8/dist/index.umd.js
@@ -21,6 +22,7 @@
 // @resource           ViewerCSS  https://fastly.jsdelivr.net/npm/viewerjs@1.11.7/dist/viewer.min.css
 // @connect            greasyfork.org
 // @connect            sleazyfork.org
+// @connect            cn-greasyfork.org
 // @grant              GM_addStyle
 // @grant              GM_deleteValue
 // @grant              GM_getResourceText
