ba78709d19a9d637943aa56dc298c2f3edfa6945
白柒 <893177236@qq.com>
2025-03-31 17:01:59 +0800

    fix: 修复访问收藏夹里的链接出现安全访问限制异常的问题

--- scripts-vite/小红书优化/src/main/XHS.ts
+++ scripts-vite/小红书优化/src/main/XHS.ts
@@ -63,10 +63,18 @@ export const XHS = {
 			function (event) {
 				utils.preventEvent(event);
 				let $click = event.target as HTMLDivElement;
-				let $url = $click.querySelector("a.cover[href]") as HTMLAnchorElement;
-				if ($url && $url.href) {
-					log.info("跳转文章: " + $url.href);
-					window.open($url.href, "_blank");
+				let $url = $click.querySelector<HTMLAnchorElement>("a.cover[href]");
+				let url = $url?.href;
+				if (url) {
+					log.info("跳转文章: " + url);
+					// 修复访问 收藏夹里的链接出现安全访问限制异常的问题
+					let urlInstance = new URL(url);
+					urlInstance.pathname = urlInstance.pathname.replace(
+						/^\/user\/profile\/[a-z0-9A-Z]+\//i,
+						"/discovery/item/"
+					);
+					url = urlInstance.toString();
+					window.open(url, "_blank");
 				} else {
 					Qmsg.error("未找到文章链接");
 				}
