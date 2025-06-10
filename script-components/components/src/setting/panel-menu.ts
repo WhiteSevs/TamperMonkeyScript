2fa26f9f1e13bedf4f71ab061ff32bb0b4cb0e61
白柒 <893177236@qq.com>
2025-06-10 16:35:12 +0800

    🦄 refactor: 调整菜单项

--- script-components/components/src/setting/panel-menu.ts
+++ script-components/components/src/setting/panel-menu.ts
@@ -8,7 +8,20 @@ import { PanelContent } from "./panel-content";
  */
 export const PanelMenu = {
 	$data: {
-		__menuOption: [] as UtilsGMMenuOption[],
+		__menuOption: [
+			{
+				key: "show_pops_panel_setting",
+				text: "⚙ 设置",
+				autoReload: false,
+				isStoreValue: false,
+				showText(text: string) {
+					return text;
+				},
+				callback: () => {
+					Panel.showPanel(PanelContent.getConfig(0));
+				},
+			},
+		] as UtilsGMMenuOption[],
 		get menuOption() {
 			return this.__menuOption;
 		},
@@ -24,29 +37,40 @@ export const PanelMenu = {
 			/* 不允许在iframe内重复注册 */
 			return;
 		}
-		GM_Menu.add([
-			{
-				key: "show_pops_panel_setting",
-				text: "⚙ 设置",
-				autoReload: false,
-				isStoreValue: false,
-				showText(text: string) {
-					return text;
-				},
-				callback: () => {
-					Panel.showPanel(PanelContent.getConfig(0));
-				},
-			},
-			...this.$data.menuOption,
-		]);
+		GM_Menu.add(this.$data.menuOption);
 	},
 	/**
 	 * 添加菜单项
+	 * @param option 菜单配置
 	 */
 	addMenuOption(option: UtilsGMMenuOption[] | UtilsGMMenuOption) {
 		if (!Array.isArray(option)) {
 			option = [option];
 		}
 		this.$data.menuOption.push(...option);
+	},
+	/**
+	 * 更新菜单项
+	 * @param option 菜单配置
+	 */
+	updateMenuOption(option: UtilsGMMenuOption[] | UtilsGMMenuOption) {
+		if (!Array.isArray(option)) {
+			option = [option];
+		}
+		option.forEach((optionItem) => {
+			let findIndex = this.$data.menuOption.findIndex((it) => {
+				return it.key === optionItem.key;
+			});
+			if (findIndex !== -1) {
+				this.$data.menuOption[findIndex] = optionItem;
+			}
+		});
+	},
+	/**
+	 * 获取菜单项
+	 * @param index 索引
+	 */
+	getMenuOption(index: number) {
+		return this.$data.menuOption[index];
 	},
 };
