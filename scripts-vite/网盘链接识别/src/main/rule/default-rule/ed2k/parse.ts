e059969e32f039f1a04649a907c2ac92a42bf9f2
白柒 <893177236@qq.com>
2025-06-13 16:44:02 +0800

    ✨ feat: 新增magnet和ed2k点击动作-文件解析
    + 移除鼠标悬停预览（若存在多个容易误触）

---
+++ scripts-vite/网盘链接识别/src/main/rule/default-rule/ed2k/parse.ts
@@ -0,0 +1,33 @@
+import { log } from "@/env";
+import { NetDiskLinkClickModeUtils } from "@/main/link-click-mode/NetDiskLinkClickMode";
+import { MetaDataParser } from "@/main/parse/MetaDataParser";
+import { ParseFileAbstract } from "@/main/parse/NetDiskParseAbstract";
+import Qmsg from "qmsg";
+
+export class NetDiskParse_ed2k extends ParseFileAbstract {
+	async init(
+		ruleIndex: number,
+		shareCode: string,
+		accessCode: AccessCodeNonNullType
+	) {
+		log.info(ruleIndex, shareCode, accessCode);
+		this.ruleIndex = ruleIndex;
+		this.shareCode = shareCode;
+		this.accessCode = accessCode;
+		const that = this;
+
+		let url = NetDiskLinkClickModeUtils.getBlankUrl(
+			"ed2k",
+			ruleIndex,
+			shareCode,
+			accessCode
+		);
+		let $loading = Qmsg.loading("正在请求Api中...");
+		let metaInfo = await MetaDataParser.parseFileMetaInfo(url);
+		$loading.close();
+		if (!metaInfo) {
+			return;
+		}
+		MetaDataParser.showFileMetaInfoDialog(metaInfo);
+	}
+}
--- scripts-vite/网盘链接识别/src/main/rule/default-rule/magnet/rule.ts
+++ scripts-vite/网盘链接识别/src/main/rule/default-rule/magnet/rule.ts
@@ -34,6 +34,15 @@ export const NetDiskRule_magnet: NetDiskRuleOption = {
 					openBlank: {
 						default: true,
 					},
+					"openBlank-closePopup": {
+						enable: true,
+					},
+					parseFile: {
+						enable: true,
+					},
+					"parseFile-closePopup": {
+						enable: true,
+					},
 				},
 			},
 			schemeUri: {
@@ -41,136 +50,6 @@ export const NetDiskRule_magnet: NetDiskRuleOption = {
 				isForwardBlankLink: true,
 				uri: "",
 			},
-			ownFormList: [
-				{
-					type: "forms",
-					text: /*html*/ `
-						<a href="https://whatslink.info/" target="_blank">元数据查询</a>
-					`,
-					forms: [
-						UISwitch(
-							"鼠标悬停预览",
-							NetDiskRule_magnet_preview.MENU_KEY,
-							NetDiskRule_magnet_preview.MENU_DEFAULT_VALUE,
-							void 0,
-							"注意：Api请求存在访问频率限制"
-						),
-					],
-				},
-			],
 		},
-	},
-	async afterRenderUrlBox(option) {
-		const that = this;
-		let generateData = GeneratePanelData(
-			NetDiskRule_magnet_preview.MENU_KEY,
-			NetDiskRule_magnet_preview.MENU_DEFAULT_VALUE
-		);
-		if (!generateData.value) {
-			return;
-		}
-		/**
-		 * 是否已经获取过信息
-		 */
-		let isQueryInfo = false;
-		let content = "正在请求Api中...";
-		let tooltip = pops.tooltip({
-			target: option.$url,
-			content: () => {
-				return content;
-			},
-			isDiffContent: true,
-			position: "follow",
-			alwaysShow: false,
-			isFixed: true,
-			showArrow: false,
-			delayCloseTime: 300,
-			triggerShowEventName: "mouseenter touchstart mousemove touchmove",
-			otherDistance: 15,
-			className: "github-tooltip",
-			showBeforeCallBack($toolTip) {
-				if (isQueryInfo) {
-					return;
-				}
-				isQueryInfo = true;
-				let url = NetDiskLinkClickModeUtils.getBlankUrl(
-					that.setting.key,
-					option.ruleIndex,
-					option.shareCode,
-					option.accessCode
-				);
-				log.info(`正在获取magnet链接信息：${url}`);
-				httpx
-					.get("https://whatslink.info/api/v1/link?url=" + url, {
-						headers: {
-							Referer: "https://whatslink.info/",
-						},
-						allowInterceptConfig: false,
-					})
-					.then((response) => {
-						let data = utils.toJSON(response.data.responseText);
-						if (!response.status) {
-							content = "请求失败";
-							if (typeof data.error === "string" && data.error.trim() !== "") {
-								content = content + "，" + data.error;
-							}
-							tooltip.toolTip.changeContent(content);
-							return;
-						}
-						content = /*html*/ `
-						<div class="wrapper">
-							<div class="title">Summary</div>
-							<div class="content">
-								<div>Resource Name: ${data.name}</div>
-								<div>Number of Files: ${data.count}</div>
-								<div>Total File Size: ${utils.formatByteToSize(data.size)}</div>
-								<div>File Type: ${data.type.toLowerCase()}</div>
-							</div>
-						</div>
-						${
-							Array.isArray(data.screenshots)
-								? /*html*/ `
-							<div class="wrapper">
-								<div class="title">Screenshots</div>
-								<div class="content">
-									<div class="image-list">
-										${data.screenshots
-											.map(
-												(screenshot: any) => /*html*/ `
-											<div class="img">
-												<img src="${screenshot.screenshot}" alt="img">
-											</div>
-										`
-											)
-											.join("")}
-										
-									</div>
-								</div>
-							</div>
-						`
-								: ""
-						}
-						`;
-						tooltip.toolTip.changeContent(content);
-					});
-			},
-			style: /*css*/ `
-				.pops-tip{
-					max-height: 500px;
-					overflow: hidden;
-				}
-				.wrapper .content{
-					text-align: left;
-				}
-				.wrapper .image-list{
-					overflow: auto;
-					display: flex;
-				}
-				.wrapper .image-list img{
-					max-width: 200px;
-					max-height: 200px;
-				}
-			`,
-		});
 	},
 };
