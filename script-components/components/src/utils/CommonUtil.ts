23443e57d44b7f82b8d00a24d162fc1cb941a7dc
白柒 <893177236@qq.com>
2025-06-13 10:46:29 +0800

    ✨ feat: 新增函数waitRemove

--- script-components/components/src/utils/CommonUtil.ts
+++ script-components/components/src/utils/CommonUtil.ts
@@ -1,8 +1,22 @@
 import DOMUtils from "@whitesev/domutils";
-import { addStyle, log } from "../base.env";
+import { addStyle, log, utils } from "../base.env";
 import { GM_getResourceText } from "ViteGM";
 
 export const CommonUtil = {
+	/**
+	 * 移除元素（未出现也可以等待出现）
+	 * @param selector 元素选择器
+	 */
+	waitRemove(...args: string[]) {
+		args.forEach((selector) => {
+			if (typeof selector !== "string") {
+				return;
+			}
+			utils.waitNodeList<NodeListOf<HTMLElement>>(selector).then((nodeList) => {
+				nodeList.forEach(($el) => $el.remove());
+			});
+		});
+	},
 	/**
 	 * 添加屏蔽CSS
 	 * @param args
