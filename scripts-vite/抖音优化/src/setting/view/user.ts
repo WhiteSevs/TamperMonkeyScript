d0f8e69b3963b1099b662c0bf9852017f95ad9e4
白柒 <893177236@qq.com>
2025-07-07 00:10:11 +0800

    ✨ feat: 移除功能用户-跳转至用户主页

--- scripts-vite/抖音优化/src/setting/view/user.ts
+++ scripts-vite/抖音优化/src/setting/view/user.ts
@@ -20,51 +20,51 @@ export const PanelUserConfig: PopsPanelContentConfig = {
 					void 0,
 					"在用户信息区域下方显示当前用户的uid"
 				),
-				UIButton(
-					"跳转至用户主页",
-					"输入用户UID自动跳转至用户主页",
-					"跳转",
-					void 0,
-					false,
-					false,
-					"default",
-					async (evt) => {
-						utils.preventEvent(evt);
-						let uid = prompt("请输入用户UID");
-						if (typeof uid !== "string") {
-							return;
-						}
-						let url = `https://www.toutiao.com/c/user/${uid}/`;
-						let urlInst = new URL(url);
-						let response = await httpx.options(url, {
-							allowInterceptConfig: false,
-							headers: {
-								"User-Agent": utils.getRandomPCUA(),
-								Host: urlInst.hostname,
-								Origin: urlInst.origin,
-								Referer: "https://www.toutiao.com/",
-							},
-						});
-						if (!response.status) {
-							log.error(response);
-							Qmsg.error("获取用户sec_uid失败", { consoleLogContent: true });
-							return;
-						}
-						let finalUrl = response.data.finalUrl;
-						let sec_uid_match = finalUrl.match(/\/user\/token\/(.+)\//);
-						if (!sec_uid_match) {
-							Qmsg.error("正则获取用户sec_uid失败", {
-								consoleLogContent: true,
-							});
-							return;
-						}
-						let sec_uid = sec_uid_match[sec_uid_match.length - 1];
-						let userHomeUrl = DouYinUrlUtils.getUserHomeUrl(sec_uid);
-						log.info(`用户sec_uid：` + sec_uid);
-						log.info(`用户主页链接：` + userHomeUrl);
-						window.open(userHomeUrl, "_blank");
-					}
-				),
+				// UIButton(
+				// 	"跳转至用户主页",
+				// 	"输入用户UID自动跳转至用户主页",
+				// 	"跳转",
+				// 	void 0,
+				// 	false,
+				// 	false,
+				// 	"default",
+				// 	async (evt) => {
+				// 		utils.preventEvent(evt);
+				// 		let uid = prompt("请输入用户UID");
+				// 		if (typeof uid !== "string") {
+				// 			return;
+				// 		}
+				// 		let url = `https://www.toutiao.com/c/user/${uid}/`;
+				// 		let urlInst = new URL(url);
+				// 		let response = await httpx.options(url, {
+				// 			allowInterceptConfig: false,
+				// 			headers: {
+				// 				"User-Agent": utils.getRandomPCUA(),
+				// 				Host: urlInst.hostname,
+				// 				Origin: urlInst.origin,
+				// 				Referer: "https://www.toutiao.com/",
+				// 			},
+				// 		});
+				// 		if (!response.status) {
+				// 			log.error(response);
+				// 			Qmsg.error("获取用户sec_uid失败", { consoleLogContent: true });
+				// 			return;
+				// 		}
+				// 		let finalUrl = response.data.finalUrl;
+				// 		let sec_uid_match = finalUrl.match(/\/user\/token\/(.+)\//);
+				// 		if (!sec_uid_match) {
+				// 			Qmsg.error("正则获取用户sec_uid失败", {
+				// 				consoleLogContent: true,
+				// 			});
+				// 			return;
+				// 		}
+				// 		let sec_uid = sec_uid_match[sec_uid_match.length - 1];
+				// 		let userHomeUrl = DouYinUrlUtils.getUserHomeUrl(sec_uid);
+				// 		log.info(`用户sec_uid：` + sec_uid);
+				// 		log.info(`用户主页链接：` + userHomeUrl);
+				// 		window.open(userHomeUrl, "_blank");
+				// 	}
+				// ),
 			],
 		},
 	],
