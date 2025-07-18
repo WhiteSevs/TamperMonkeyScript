7bd61d629dfd2e8261fe66d32044edb35407533e
白柒 <893177236@qq.com>
2025-07-18 14:54:55 +0800

    fix: 修复【屏蔽】评论区的贵宾席

--- scripts-vite/抖音优化/src/main/live/DouYinLiveBlock.ts
+++ scripts-vite/抖音优化/src/main/live/DouYinLiveBlock.ts
@@ -51,8 +51,7 @@ export const DouYinLiveBlock_ChatRoom = {
 				"#chatroom > div > div:has(#audiencePanelScrollId)",
 				'#chatroom > div > div:has([data-e2e="live-room-audience"])',
 				// Firefox上的CSS，多了个pace-island
-				'#chatroom > pace-island > div > div > div:has([data-e2e="live-room-audience"])',
-				"#chatroom > pace-island > .chatroom_close+div > div:first-child"
+				'#chatroom > pace-island > div > div:has([data-e2e="live-room-audience"])'
 			),
 		];
 	},
