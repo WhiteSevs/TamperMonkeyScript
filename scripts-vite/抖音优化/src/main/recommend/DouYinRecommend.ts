7dd9ec866a02d73b285562c64a279ded877cdeec
白柒 <893177236@qq.com>
2025-07-19 12:19:10 +0800

    🎈 perf: 优化 推荐-自动连播

--- scripts-vite/抖音优化/src/main/recommend/DouYinRecommend.ts
+++ scripts-vite/抖音优化/src/main/recommend/DouYinRecommend.ts
@@ -1,6 +1,7 @@
 import { $, DOMUtils, log, utils } from "@/env";
 import { DouYinRouter } from "@/router/DouYinRouter";
 import { Panel } from "@components/setting/panel";
+import { CommonUtil } from "@components/utils/CommonUtil";
 
 export const DouYinRecommend = {
 	init() {
@@ -9,35 +10,73 @@ export const DouYinRecommend = {
 		});
 	},
 	/**
-	 * 自动连续播放
+	 * 自动连播
 	 */
 	automaticContinuousPlayback() {
-		log.info(`自动连续播放`);
+		log.info(`自动连播`);
+		const attrFlagName = "data-automaticContinuousPlayback";
+		/**
+		 * 获取当前播放的视频
+		 */
+		let queryActiveVideo = (withAttr: boolean = false) => {
+			return $<HTMLVideoElement>(
+				`.page-recommend-container [data-e2e="feed-active-video"] video${
+					withAttr ? `:not([${attrFlagName}])` : ""
+				}`
+			);
+		};
+		let switchActiveVideo = () => {
+			let keydownEvent = new KeyboardEvent("keydown", {
+				bubbles: true,
+				cancelable: true,
+				key: "ArrowDown",
+				code: "ArrowDown",
+				keyCode: 40,
+				which: 40,
+			});
+			document.body.dispatchEvent(keydownEvent);
+		};
 		let lockFn = new utils.LockFunction(() => {
 			if (!DouYinRouter.isRecommend()) {
+				// 必须在推荐页面
 				return;
 			}
-			let $activeVideo = $<HTMLVideoElement>(
-				`.page-recommend-container [data-e2e="feed-active-video"] video:not([data-automaticContinuousPlayback])`
-			);
+			let $activeVideo = queryActiveVideo();
 			if (!$activeVideo) {
 				return;
 			}
-			$activeVideo.setAttribute("data-automaticContinuousPlayback", "true");
+			if ($activeVideo.hasAttribute(attrFlagName)) {
+				return;
+			}
+			$activeVideo.setAttribute(attrFlagName, "true");
+			let intervalId: number;
 			DOMUtils.on(
 				$activeVideo,
 				"ended",
 				(evt) => {
-					let keydownEvent = new KeyboardEvent("keydown", {
-						bubbles: true,
-						cancelable: true,
-						key: "ArrowDown",
-						code: "ArrowDown",
-						keyCode: 40,
-						which: 40,
-					});
-					document.body.dispatchEvent(keydownEvent);
 					log.success(`视频播放完毕，切换至下一个视频`);
+					CommonUtil.interval(
+						(isTimeout) => {
+							if (isTimeout) {
+								log.error(`切换视频超时，切换失败`);
+								return;
+							}
+							let $switchActiveVideo = queryActiveVideo(false);
+							if ($switchActiveVideo == null) {
+								log.error(`切换视频失败，没有找到当前正在播放的视频`);
+								return;
+							}
+							if ($activeVideo !== $switchActiveVideo) {
+								log.success("切换视频成功");
+								return false;
+							}
+							// 切换失败
+							// 继续切换
+							switchActiveVideo();
+						},
+						500,
+						3000
+					);
 				},
 				{ capture: true }
 			);
