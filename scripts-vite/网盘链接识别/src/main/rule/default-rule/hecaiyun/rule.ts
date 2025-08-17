84892cad8b7ed9893f1934b909ef137ec1cd0b7a
白柒 <893177236@qq.com>
2025-08-17 21:25:37 +0800

    ✨ feat: 移动云盘适配链接

--- scripts-vite/网盘链接识别/src/main/rule/default-rule/hecaiyun/rule.ts
+++ scripts-vite/网盘链接识别/src/main/rule/default-rule/hecaiyun/rule.ts
@@ -13,6 +13,17 @@ export const NetDiskRule_hecaiyun: NetDiskRuleOption = {
 			copyUrl: "https://caiyun.139.com/m/i?{#shareCode#}\n密码：{#accessCode#}",
 		},
 		{
+			link_innerText: `caiyun.139.com/w/i/([a-zA-Z0-9_-]{8,14})([\\s\\S]{0,{#matchRange-text-before#}}(密码|访问码|提取码)[\\s\\S]{0,{#matchRange-text-after#}}[0-9a-zA-Z]{4}|)`,
+			link_innerHTML: `caiyun.139.com/w/i/([a-zA-Z0-9_-]{8,14})([\\s\\S]{0,{#matchRange-html-before#}}(密码|访问码|提取码)[\\s\\S]{0,{#matchRange-html-after#}}[0-9a-zA-Z]{4}|)`,
+			shareCode: /caiyun\.139\.com\/w\/i\/([a-zA-Z0-9_\-]{8,14})/gi,
+			shareCodeNeedRemoveStr: /caiyun\.139\.com\/w\/i\//gi,
+			checkAccessCode: /(密码|访问码|提取码)[\s\S]+/g,
+			accessCode: /([0-9a-zA-Z]{4})/gi,
+			uiLinkShow: "caiyun.139.com/w/i/{#shareCode#} 提取码: {#accessCode#}",
+			blank: "https://caiyun.139.com/w/i/{#shareCode#}",
+			copyUrl: "https://caiyun.139.com/w/i/{#shareCode#}\n密码：{#accessCode#}",
+		},
+		{
 			link_innerText: `yun.139.com/link/w/i/([a-zA-Z0-9_-]{8,14})([\\s\\S]{0,{#matchRange-text-before#}}(密码|访问码|提取码)[\\s\\S]{0,{#matchRange-text-after#}}[0-9a-zA-Z]{4}|)`,
 			link_innerHTML: `yun.139.com/link/w/i/([a-zA-Z0-9_-]{8,14})([\\s\\S]{0,{#matchRange-html-before#}}(密码|访问码|提取码)[\\s\\S]{0,{#matchRange-html-after#}}[0-9a-zA-Z]{4}|)`,
 			shareCode: /yun\.139\.com\/link\/w\/i\/([a-zA-Z0-9_\-]{8,14})/gi,
@@ -21,8 +32,7 @@ export const NetDiskRule_hecaiyun: NetDiskRuleOption = {
 			accessCode: /([0-9a-zA-Z]{4})/gi,
 			uiLinkShow: "yun.139.com/link/w/i/{#shareCode#} 提取码: {#accessCode#}",
 			blank: "https://yun.139.com/link/w/i/{#shareCode#}",
-			copyUrl:
-				"https://yun.139.com/link/w/i/{#shareCode#}\n密码：{#accessCode#}",
+			copyUrl: "https://yun.139.com/link/w/i/{#shareCode#}\n密码：{#accessCode#}",
 		},
 	],
 
