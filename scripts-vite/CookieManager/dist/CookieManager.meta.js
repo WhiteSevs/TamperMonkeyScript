970bcaba29f54b9aa4ebfb2ff6f857a3aef55c35
白柒 <893177236@qq.com>
2025-08-24 11:09:51 +0800

    release: 2025.8.24
    - ✨ feat: 禁用双击设置面板列表弹出搜索配置弹出

--- scripts-vite/CookieManager/dist/CookieManager.meta.js
+++ scripts-vite/CookieManager/dist/CookieManager.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CookieManager
 // @namespace    https://github.com/WhiteSevs/TamperMonkeyScript
-// @version      2025.8.21
+// @version      2025.8.24
 // @author       WhiteSevs
 // @description  简单而强大的Cookie编辑器，允许您快速创建、编辑和删除Cookie
 // @license      GPL-3.0-only
@@ -9,9 +9,9 @@
 // @supportURL   https://github.com/WhiteSevs/TamperMonkeyScript/issues
 // @match        *://*/*
 // @require      https://fastly.jsdelivr.net/gh/WhiteSevs/TamperMonkeyScript@86be74b83fca4fa47521cded28377b35e1d7d2ac/lib/CoverUMD/index.js
-// @require      https://fastly.jsdelivr.net/npm/@whitesev/utils@2.7.4/dist/index.umd.js
-// @require      https://fastly.jsdelivr.net/npm/@whitesev/domutils@1.6.4/dist/index.umd.js
-// @require      https://fastly.jsdelivr.net/npm/@whitesev/pops@2.3.5/dist/index.umd.js
+// @require      https://fastly.jsdelivr.net/npm/@whitesev/utils@2.7.5/dist/index.umd.js
+// @require      https://fastly.jsdelivr.net/npm/@whitesev/domutils@1.6.5/dist/index.umd.js
+// @require      https://fastly.jsdelivr.net/npm/@whitesev/pops@2.3.6/dist/index.umd.js
 // @require      https://fastly.jsdelivr.net/npm/qmsg@1.4.0/dist/index.umd.js
 // @require      https://fastly.jsdelivr.net/gh/WhiteSevs/TamperMonkeyScript@886625af68455365e426018ecb55419dd4ea6f30/lib/CryptoJS/index.js
 // @connect      *
--- scripts-vite/CookieManager/src/entrance.ts
+++ scripts-vite/CookieManager/src/entrance.ts
@@ -5,8 +5,14 @@ import { CookieRuleController } from "./main/CookieRuleController";
 import { Panel } from "@components/setting/panel";
 import { Component_Rule } from "./setting/view/rule";
 import { Component_Common } from "./setting/view/general";
+import { PanelMenu } from "@components/setting/panel-menu";
 
 PanelContent.addContentConfig([Component_Common, Component_Rule]);
+let defaultMenuOption = PanelMenu.getMenuOption(0);
+defaultMenuOption.callback = () => {
+	Panel.showPanel(PanelContent.getConfig(0), void 0, void 0, true);
+};
+PanelMenu.updateMenuOption(defaultMenuOption);
 Panel.init();
 CookieManagerView.init();
 CookieRule.init();
