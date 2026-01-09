2b72dd95894050b0c4a1747a9642ae9956f44c34
白柒 <893177236@qq.com>
2026-01-10 00:43:04 +0800

    ✨ feat: 新增switch组件的右键菜单配置

---
+++ script-components/components/src/utils/SwitchShortCut.ts
@@ -0,0 +1,47 @@
+import { log } from "./../base.env";
+import { ShortCut, type ShortCutOption } from "./ShortCut";
+import { Panel } from "./../setting/panel";
+
+export type SwitchShortCutExecMenuConfig = { key: string; name: string };
+export const SwitchShortCut = (KEY: string) => {
+  return {
+    shortCut: new ShortCut(KEY),
+    /**
+     * 待执行的键
+     * @private
+     */
+    execConfig: [] as SwitchShortCutExecMenuConfig[],
+    init() {
+      const shortCutOption: ShortCutOption = {};
+      this.execConfig.forEach((it) => {
+        const { key, name } = it;
+        shortCutOption[key] = {
+          target: "window",
+          callback() {
+            log.info(`快捷键 ==> ${name ?? key}`);
+            const enable = Panel.getValue(key);
+            Panel.setValue(key, !!!enable);
+          },
+        };
+      });
+      this.shortCut.initGlobalKeyboardListener(shortCutOption);
+    },
+    /**
+     * 添加
+     * @param key 待执行的快捷键配置
+     */
+    add(key: SwitchShortCutExecMenuConfig) {
+      if (this.execConfig.includes(key)) return;
+      this.execConfig.push(key);
+    },
+    /**
+     * 删除
+     * @param key 需移除待执行的快捷键配置
+     */
+    delete(key: SwitchShortCutExecMenuConfig) {
+      const findIndex = this.execConfig.findIndex((item) => item === key);
+      if (findIndex === -1) return;
+      this.execConfig.splice(findIndex, 1);
+    },
+  };
+};
