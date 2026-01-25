4003b841523a4a9a3738e2ce462a250c2e4fcd6b
白柒 <893177236@qq.com>
2026-01-25 18:08:47 +0800

    feat(RuleEditView): 增强规则编辑视图功能并优化代码结构

--- script-components/components/src/utils/RuleEditView.ts
+++ script-components/components/src/utils/RuleEditView.ts
@@ -1,17 +1,33 @@
 import type { PopsConfirmConfig } from "@whitesev/pops/dist/types/src/components/confirm/types/index";
-import { pops, utils } from "../base.env";
+import { DOMUtils, pops, utils } from "../base.env";
 
 type RuleEditViewOption<T> = {
-  /** 标题 */
-  title: string;
-  /** 获取当前的数据 */
-  data: () => IPromise<T>;
-  /** 弹窗关闭的回调 */
-  dialogCloseCallBack: (isSubmit: boolean) => IPromise<void>;
   /**
-   * <form>内的html内容
+   * 标题
    */
-  getView: (data: T) => IPromise<DocumentFragment>;
+  title: string;
+  /**
+   * 获取当前的数据
+   */
+  data: () => IPromise<T>;
+  /**
+   * 弹窗关闭的回调
+   */
+  dialogCloseCallBack?: (
+    /**
+     * 当前是否是提交表单内容时触发的回调
+     */
+    isSubmit: boolean
+  ) => IPromise<void>;
+  /**
+   * \<form\>内渲染的元素
+   */
+  getView: (
+    /**
+     * 数据
+     */
+    data: T
+  ) => IPromise<DocumentFragment | HTMLElement | (HTMLElement | DocumentFragment)[]>;
   /**
    * 自定义的style
    */
@@ -19,14 +35,19 @@ type RuleEditViewOption<T> = {
   /**
    * 当提交表单时触发的回调函数
    */
-  onsubmit: /**
+  onsubmit /**
    * @param event
    * @returns
    * + true 校验通过
    * + false 校验失败
-   */
-  (
+   */: (
+    /**
+     * 表单元素
+     */
     $form: HTMLFormElement,
+    /**
+     * 数据
+     */
     data: T
   ) => IPromise<{
     success: boolean;
@@ -55,18 +76,18 @@ class RuleEditView<T> {
    * 显示视图
    */
   async showView() {
-    let $dialog = pops.confirm({
+    const $dialog = pops.confirm({
       title: {
         text: this.option.title,
         position: "center",
       },
       content: {
         text: /*html*/ `
-                    <form class="rule-form-container" onsubmit="return false">
-                        <ul class="rule-form-ulist"></ul>
-                        <input type="submit" style="display: none;" />
-                    </form>
-                    `,
+        <form class="rule-form-container" onsubmit="return false">
+            <ul class="rule-form-ulist"></ul>
+            <input type="submit" style="display: none;" />
+        </form>
+        `,
         html: true,
       },
       btn: utils.assign(
@@ -85,80 +106,83 @@ class RuleEditView<T> {
         enable: true,
       },
       style: /*css*/ `
-                ${pops.config.cssText.panelCSS}
-                
-                .rule-form-container {
-                    
-                }
-                .rule-form-container li{
-                    display: flex;
-                    align-items: center;
-                    justify-content: space-between;
-                    padding: 5px 20px;
-                    gap: 10px;
-                }
-				.rule-form-ulist-dynamic{
-					--button-margin-top: 0px;
-					--button-margin-right: 0px;
-					--button-margin-bottom: 0px;
-					--button-margin-left: 0px;
-					display: flex;
-					flex-direction: column;
-					align-items: flex-start;
-					padding: 5px 0px 5px 20px;
-				}
-				.rule-form-ulist-dynamic__inner{
-					width: 100%;
-				}
-				.rule-form-ulist-dynamic__inner-container{
-					display: flex;
-					align-items: center;
-				}
-				.dynamic-forms{
-					width: 100%;
-				}
-                .pops-panel-item-left-main-text{
-                    max-width: 150px;
-                }
-                .pops-panel-item-right-text{
-                    padding-left: 30px;
-                }
-                .pops-panel-item-right-text,
-                .pops-panel-item-right-main-text{
-                    text-overflow: ellipsis;
-                    overflow: hidden;
-                    white-space: nowrap;
-                }
-				.pops-panel-item-left-desc-text{
-					line-height: normal;
-					margin-top: 6px;
-					font-size: 0.8em;
-					color: rgb(108, 108, 108);
-				}
+      ${pops.config.cssText.panelCSS}
+      
+      .rule-form-container {
+          
+      }
+      .rule-form-container li{
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding: 5px 20px;
+          gap: 10px;
+      }
+      .rule-form-ulist-dynamic{
+        --button-margin-top: 0px;
+        --button-margin-right: 0px;
+        --button-margin-bottom: 0px;
+        --button-margin-left: 0px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 5px 0px 5px 20px;
+      }
+      .rule-form-ulist-dynamic__inner{
+        width: 100%;
+      }
+      .rule-form-ulist-dynamic__inner-container{
+        display: flex;
+        align-items: center;
+      }
+      .dynamic-forms{
+        width: 100%;
+      }
+      .pops-panel-item-left-main-text{
+          max-width: 150px;
+      }
+      .pops-panel-item-right-text{
+          padding-left: 30px;
+      }
+      .pops-panel-item-right-text,
+      .pops-panel-item-right-main-text{
+          text-overflow: ellipsis;
+          overflow: hidden;
+          white-space: nowrap;
+      }
+      .pops-panel-item-left-desc-text{
+        line-height: normal;
+        margin-top: 6px;
+        font-size: 0.8em;
+        color: rgb(108, 108, 108);
+      }
 
-                ${this.option?.style ?? ""}
-            `,
+      ${this.option?.style ?? ""}
+      `,
       width: typeof this.option.width === "function" ? this.option.width() : window.innerWidth > 500 ? "500px" : "88vw",
       height:
         typeof this.option.height === "function" ? this.option.height() : window.innerHeight > 500 ? "500px" : "80vh",
     });
 
-    let $form = $dialog.$shadowRoot.querySelector<HTMLFormElement>(".rule-form-container")!;
-    let $submit = $dialog.$shadowRoot.querySelector<HTMLInputElement>("input[type=submit]")!;
-    let $ulist = $dialog.$shadowRoot.querySelector<HTMLUListElement>(".rule-form-ulist")!;
-    let view = await this.option.getView(await this.option.data());
-    $ulist.appendChild(view);
+    const $form = $dialog.$shadowRoot.querySelector<HTMLFormElement>(".rule-form-container")!;
+    const $submitInput = $dialog.$shadowRoot.querySelector<HTMLInputElement>("input[type=submit]")!;
+    const $ulist = $dialog.$shadowRoot.querySelector<HTMLUListElement>(".rule-form-ulist")!;
+    const view = await this.option.getView(await this.option.data());
+    DOMUtils.append($ulist, view);
     /**
      * 保存配置的回调
      */
     const submitSaveOption = async () => {
-      let result = await this.option.onsubmit($form, await this.option.data());
+      const result = await this.option.onsubmit($form, await this.option.data());
       if (!result.success) {
         return;
       }
       $dialog.close();
-      await this.option.dialogCloseCallBack(true);
+      if (typeof this.option.dialogCloseCallBack === "function") {
+        await this.option.dialogCloseCallBack(true);
+      }
     };
+    return $dialog;
   }
 }
 
