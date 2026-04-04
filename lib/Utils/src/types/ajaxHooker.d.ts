d38a24866f4f857c5a71cb04f99f7db8fd63122d
白柒 <893177236@qq.com>
2026-04-04 18:43:04 +0800

    feat: 完善ajaxHooker类型提示

--- lib/Utils/src/types/ajaxHooker.d.ts
+++ lib/Utils/src/types/ajaxHooker.d.ts
@@ -1,5 +1,7 @@
 import type { HttpxHeaders, HttpxMethod, HttpxStatus } from "./Httpx";
 
+export declare type UtilsAjaxHookType = "xhr" | "fetch";
+
 /** 请求的response配置 */
 export declare interface UtilsAjaxHookResponseOptions {
   /**
@@ -24,12 +26,92 @@ export declare interface UtilsAjaxHookResponseOptions {
   responseText?: string;
 }
 
-/** hook请求的配置 */
-export declare interface UtilsAjaxHookRequestOptions {
+/**
+ * 额外属性
+ *
+ * 你可以在request对象上添加新属性，只要该属性被对应的type所支持，修改就能直接生效。
+ *
+ * 这些属性未作为request对象的标准属性，是因为xhr和fetch的接口设计有较大差异，库不主动读取这些属性的值，仅保留修改能力。
+ */
+export declare type UtilsAjaxHookRequestExtraOptions_xhr = {
+  /**
+   * The XMLHttpRequest property **`responseType`** is an enumerated string value specifying the type of data contained in the response.
+   *
+   * [MDN Reference](https://developer.mozilla.org/docs/Web/API/XMLHttpRequest/responseType)
+   */
+  responseType: XMLHttpRequest["responseType"];
+  /**
+   * The **`XMLHttpRequest.timeout`** property is an unsigned long representing the number of milliseconds a request can take before automatically being terminated. The default value is 0, which means there is no timeout. Timeout shouldn't be used for synchronous XMLHttpRequests requests used in a document environment or it will throw an InvalidAccessError exception. When a timeout happens, a timeout event is fired.
+   *
+   * [MDN Reference](https://developer.mozilla.org/docs/Web/API/XMLHttpRequest/timeout)
+   */
+  timeout: XMLHttpRequest["timeout"];
+  /**
+   * The **`XMLHttpRequest.withCredentials`** property is a boolean value that indicates whether or not cross-site Access-Control requests should be made using credentials such as cookies, authentication headers or TLS client certificates. Setting withCredentials has no effect on same-origin requests.
+   *
+   * [MDN Reference](https://developer.mozilla.org/docs/Web/API/XMLHttpRequest/withCredentials)
+   */
+  withCredentials: XMLHttpRequest["withCredentials"];
+};
+
+/**
+ * 额外属性
+ *
+ * 你可以在request对象上添加新属性，只要该属性被对应的type所支持，修改就能直接生效。
+ *
+ * 这些属性未作为request对象的标准属性，是因为xhr和fetch的接口设计有较大差异，库不主动读取这些属性的值，仅保留修改能力。
+ */
+export declare type UtilsAjaxHookRequestExtraOptions_fetch = {
+  /**
+   * 一个字符串，指示请求如何与浏览器的缓存交互以设置请求的缓存。
+   */
+  cache: RequestInit["cache"];
+  /**
+   * 一个字符串，指示凭据是始终、从不还是仅在发送到同源 URL 时随请求一起发送。设置请求的凭据。
+   */
+  credentials: RequestInit["credentials"];
+  /**
+   * 要按请求获取的资源的加密哈希值。设置请求的完整性。
+   */
+  integrity: RequestInit["integrity"];
+  /**
+   * 用于设置请求保持活动的布尔值。
+   */
+  keepalive: RequestInit["keepalive"];
+  /**
+   * 一个字符串来指示请求是使用CORS，还是仅限于相同的Origin URL。设置请求模式。
+   */
+  mode: RequestInit["mode"];
+  /**
+   *
+   */
+  priority: RequestInit["priority"];
+  /**
+   * 指示请求是否遵循重定向、在遇到重定向时导致错误或返回重定向（以不透明方式）的字符串。设置请求的重定向。
+   */
+  redirect: RequestInit["redirect"];
+  /**
+   * 一个字符串，其值为同源 URL、“about:client”或空字符串，用于设置请求的引荐来源网址。
+   */
+  referrer: RequestInit["referrer"];
+  /**
+   * 用于设置请求的referrerPolicy 的引用策略。
+   */
+  referrerPolicy: RequestInit["referrerPolicy"];
+  /**
+   * 用于设置请求信号的 AbortSignal。
+   */
+  signal: RequestInit["signal"];
+};
+
+/**
+ * hook到的请求的配置
+ */
+export declare type UtilsAjaxHookRequestOption<T extends UtilsAjaxHookType = UtilsAjaxHookType> = {
   /**
    * 只读属性。一个字符串，表明请求类型是xhr还是fetch
    */
-  type: "xhr";
+  readonly type: T;
   /**
    * 请求的Url
    */
@@ -63,8 +145,17 @@ export declare interface UtilsAjaxHookRequestOptions {
   /**
    * 只读属性。异步请求为true，同步请求为false，异步特性无法作用于同步请求
    */
-  async: boolean;
-}
+  readonly async: boolean;
+};
+
+/**
+ * hook请求的配置
+ */
+export declare type UtilsAjaxHookRequestOptions<T extends UtilsAjaxHookType = UtilsAjaxHookType> = T extends "xhr"
+  ? UtilsAjaxHookRequestExtraOptions_xhr & UtilsAjaxHookRequestOption<T>
+  : T extends "fetch"
+    ? UtilsAjaxHookRequestExtraOptions_fetch & UtilsAjaxHookRequestOption<T>
+    : UtilsAjaxHookRequestOption<T>;
 
 /** 过滤规则配置 */
 export declare interface UtilsAjaxHookFilterOptions {
@@ -136,7 +227,7 @@ export declare interface UtilsAjaxHookResult {
     ) => void | undefined | null | Promise<void | undefined | null>
   ): {
     /**
-     * 该函数是额外添加的，原版仅返回空值
+     * 移除添加的hook（该函数是额外添加的，原版仅返回空值）
      */
     remove: () => boolean;
   };
@@ -154,7 +245,7 @@ export declare interface UtilsAjaxHookResult {
    */
   filter(filterOptions: UtilsAjaxHookFilterOptions[]): {
     /**
-     * 该函数是额外添加的，原版仅返回空值
+     * 移除添加的filter（该函数是额外添加的，原版仅返回空值）
      */
     remove: () => void;
   };
