651c3fedddcedc273d94db9709f26d78560faaec
白柒 <893177236@qq.com>
2026-04-18 17:19:36 +0800

    feat: add dev window variable

--- script-components/components/src/setting/panel-storage.ts
+++ script-components/components/src/setting/panel-storage.ts
@@ -1,6 +1,11 @@
+import { unsafeWindow } from "ViteGM";
 import { StorageUtils } from "./../utils/StorageUtils";
 import { KEY } from "./panel-config";
 
 const PopsPanelStorageApi = new StorageUtils(KEY);
+
+if (import.meta.env.DEV) {
+  Reflect.set(unsafeWindow, "PopsPanelStorageApi", PopsPanelStorageApi);
+}
 
 export { PopsPanelStorageApi };
