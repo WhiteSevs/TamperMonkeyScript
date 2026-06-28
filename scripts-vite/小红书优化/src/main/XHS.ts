b1652af7a170d1271003d2023496ab5027b380d5
白柒 <893177236@qq.com>
2026-06-28 23:41:01 +0800

    release(小红书优化): 2026.6.28
    - fix: 修复新标签页打开未正确处理文章为直播状态的问题
    - perf: 优化注册脚本菜单

--- scripts-vite/小红书优化/src/main/XHS.ts
+++ scripts-vite/小红书优化/src/main/XHS.ts
@@ -58,25 +58,37 @@ export const XHS = {
    */
   openBlankArticle() {
     log.success("新标签页打开文章");
-    const callback = (event: MouseEvent | PointerEvent, $click: HTMLElement) => {
-      if (!Panel.getValue("pc-xhs-open-blank-article")) return;
-      DOMUtils.preventEvent(event);
-      const $url = $click.querySelector<HTMLAnchorElement>("a.cover[href]");
-      let url = $url?.href;
-      if (url) {
+    const listener = DOMUtils.on(
+      document,
+      "click",
+      ".feeds-container .note-item",
+      (event: MouseEvent | PointerEvent, $click: HTMLElement) => {
+        if (!Panel.getValue("pc-xhs-open-blank-article")) return;
+        const $url = $click.querySelector<HTMLAnchorElement>("a.cover[href]");
+        let url = $url?.href;
+        if (!url) {
+          const $badge = $click.querySelector<HTMLElement>(".xhs-live-note-item-badge");
+          if ($badge && DOMUtils.text($badge).includes("直播中")) {
+            // 直播的文章是默认新标签页打开
+            return;
+          }
+        }
+        DOMUtils.preventEvent(event);
+        if (!url) {
+          Qmsg.error("未找到文章链接");
+          return;
+        }
         log.info("跳转文章: " + url);
-        // 修复访问 收藏夹里的链接出现安全访问限制异常的问题
+        // 修复访问收藏夹里的链接时出现的安全访问限制异常的问题
         const urlInst = new URL(url);
         urlInst.pathname = urlInst.pathname.replace(/^\/user\/profile\/[a-z0-9A-Z]+\//i, "/discovery/item/");
         url = urlInst.toString();
         window.open(url, "_blank");
-      } else {
-        Qmsg.error("未找到文章链接");
+      },
+      {
+        capture: true,
       }
-    };
-    const listener = DOMUtils.on(document, "click", ".feeds-container .note-item", callback, {
-      capture: true,
-    });
+    );
     return [
       () => {
         listener.off();
